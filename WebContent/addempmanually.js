e0b6fc236eaf55e947f68d29c077e321fa5cc3bd
nitin2402 <nitinsaini2402@gmail.com>
2015-06-26 18:40:02 +0530

    SD-js for addempmanually.jsp

---
+++ WebContent/addempmanually.js
@@ -0,0 +1,10 @@
+$(function() {
+		$('.last').click(
+				function(event) {
+					var email = 'chinu.mohanty@accenture.com';
+					var subject = 'Feedback';
+					var emailBody = 'Hi Chinu,';
+					window.location = 'mailto:' + email + '?subject=' + subject
+							+ '&body=' + emailBody;
+				});
+	});
