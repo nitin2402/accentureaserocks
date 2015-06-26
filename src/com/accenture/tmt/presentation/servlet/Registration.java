120f713a6121adb9bfbc23736b7fa4be3d0cfeeb
nitin2402 <nitinsaini2402@gmail.com>
2015-06-26 18:24:30 +0530

    RTM: servlet for signup

--- src/com/accenture/tmt/presentation/servlet/Registration.java
+++ src/com/accenture/tmt/presentation/servlet/Registration.java
@@ -1,16 +1,14 @@
 package com.accenture.tmt.presentation.servlet;
 
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.accenture.tmt.manager.LoginController;
-
-
+import com.accenture.tmt.common.CONSTANTS;
+import com.accenture.tmt.manager.SignupController;
+import com.accenture.tmt.presentation.dto.RegistrationDTO;
 
 /**
  * Servlet implementation class Registration
@@ -39,22 +37,37 @@ public class Registration extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String name = request.getParameter("name");
-		String userName = request.getParameter("newuser");
-		String password = request.getParameter("password");
+		String userName = request.getParameter(CONSTANTS.NEW_USER);
+		String password = request.getParameter(CONSTANTS.NEW_USER_PASS_WORD);
+		String employeeid =request.getParameter(CONSTANTS.NEW_EMPLOYEE_ID);
 		
-		LoginController reg = new LoginController();
-	    boolean a=	reg.UserRegistration(name, userName, password);
-		
-		if(a==true)
+	RegistrationDTO registerCred = new RegistrationDTO();
+	registerCred.setUsername(userName);
+	registerCred.setPassword(password);
+	registerCred.setEmployeeid(employeeid);
+	SignupController registerCtrl = new SignupController();
+	 int returnuserregistered = registerCtrl.UserRegistration(registerCred);
+	 if(returnuserregistered==0)
 		{
-			response.sendRedirect("home.jsp?msg=User added ! Login to continue");
+			response.sendRedirect("login.jsp?msg=User added ! Login to continue");
 		}
-		else
+	 else if(returnuserregistered==1)
 		{
-			response.sendRedirect("home.jsp?msg=Registration failed");
+			response.sendRedirect("login.jsp?msg=Registration failed...User is already signed up..Please Login");
 		}
-		
+	 else if(returnuserregistered==2)
+	 {
+		 response.sendRedirect("login.jsp?msg=Registration failed...already existing username"); 
+	 }
+	 else if(returnuserregistered==3)
+	 {
+		 response.sendRedirect("login.jsp?msg=Registration failed...Employeeid does not exist"); 
+	 }
+	 
+	 else
+	 {
+		 response.sendRedirect("login.jsp?msg=Registration failed..."); 
+	 }
 	}
 
 }
