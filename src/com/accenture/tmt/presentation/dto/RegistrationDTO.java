333b9be450fd4253969cd1c7241f4199c11dbc00
nitin2402 <nitinsaini2402@gmail.com>
2015-06-26 18:20:18 +0530

    RTM: presentation DTO for signup

---
+++ src/com/accenture/tmt/presentation/dto/RegistrationDTO.java
@@ -0,0 +1,33 @@
+package com.accenture.tmt.presentation.dto;
+
+public class RegistrationDTO {
+
+	private String username;
+	private String password;
+	private String employeeid;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmployeeid() {
+		return employeeid;
+	}
+	public void setEmployeeid(String employeeid) {
+		this.employeeid = employeeid;
+	}
+	
+	
+
+	
+	
+
+}
