e8139fc3e9616e218eb72b09a297a4b89249c6b1
nitin2402 <nitinsaini2402@gmail.com>
2015-06-26 18:42:48 +0530

    SD-servlet to choose manually/from excel for add employee

---
+++ src/com/accenture/tmt/presentation/servlet/AddEmpChoice.java
@@ -0,0 +1,46 @@
+package com.accenture.tmt.presentation.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddEmpManual
+ */
+public class AddEmpChoice extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddEmpChoice() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+          String select = request.getParameter("select");
+		
+		if(select=="addfromexcel" || select .equals("addfromexcel"))
+			response.sendRedirect("addempexcel.jsp");
+		if(select=="addmanually" || select .equals("addmanually"))
+			response.sendRedirect("addempmanually.jsp");
+		
+	}
+
+}
