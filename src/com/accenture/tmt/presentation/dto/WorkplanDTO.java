b80da0c9db70f75fae1cbdd558259fac6edbdd51
nitin2402 <nitinsaini2402@gmail.com>
2015-06-26 17:38:32 +0530

    sachin: workplanDTO

---
+++ src/com/accenture/tmt/presentation/dto/WorkplanDTO.java
@@ -0,0 +1,42 @@
+package com.accenture.tmt.presentation.dto;
+
+public class WorkplanDTO {
+	
+	private String userName ;
+	private int nASE ;
+	private int nSE ;
+	private int nSSE ;
+	private String comment ;
+	
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public int getnASE() {
+		return nASE;
+	}
+	public void setnASE(int nASE) {
+		this.nASE = nASE;
+	}
+	public int getnSE() {
+		return nSE;
+	}
+	public void setnSE(int nSE) {
+		this.nSE = nSE;
+	}
+	public int getnSSE() {
+		return nSSE;
+	}
+	public void setnSSE(int nSSE) {
+		this.nSSE = nSSE;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
