03699c78efd0b4a523b63aa2137a0aa2952d1e55
nitin2402 <nitinsaini2402@gmail.com>
2015-06-29 11:58:03 +0530

    AddTeamManually - suchi

---
+++ src/com/accenture/tmt/presentation/dto/TeamDTO.java
@@ -0,0 +1,34 @@
+package com.accenture.tmt.presentation.dto;
+
+public class TeamDTO {
+	private String Teamid;
+	private String TeamName;
+	private String TeamDesc;
+	private String ModuleId;
+	public String getTeamid() {
+		return Teamid;
+	}
+	public void setTeamid(String Teamid) {
+		this.Teamid = Teamid;
+	}
+	public String getTeamName() {
+		return TeamName;
+	}
+	public void setTeamName(String TeamName) {
+		this.TeamName = TeamName;
+	}
+	public String getTeamDesc() {
+		return TeamDesc;
+	}
+	public void setTeamDesc(String TeamDesc) {
+		this.TeamDesc = TeamDesc;
+	}
+	public String getModuleId() {
+		return ModuleId;
+	}
+	public void setModuleId(String ModuleId) {
+		this.ModuleId = ModuleId;
+	}
+	
+	
+}
