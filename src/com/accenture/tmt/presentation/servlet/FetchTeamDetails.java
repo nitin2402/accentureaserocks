b9eab21efdfc9ba611f15887711f29863f9eabf9
nitin2402 <nitinsaini2402@gmail.com>
2015-07-01 13:22:04 +0530

    NS:Edit & Delete

--- src/com/accenture/tmt/presentation/servlet/FetchTeamDetails.java
+++ src/com/accenture/tmt/presentation/servlet/FetchTeamDetails.java
@@ -48,12 +48,11 @@ public class FetchTeamDetails extends HttpServlet {
 		// System.out.println(li);
 		// System.out.println(li.getItem(0));
 		String exp = "<form action=\"EditTeam\"><div id=\"scrollable\"> <table class= \"table1\" border=\"1\" >"
-				+ "<tr style=\"font-weight:bold\"><td></td><td>Team Name</td><td>Team ID</td><td>Module Name </td><td>Project Name</td><td>Team Description</td></tr>";
+				+ "<tr style=\"font-weight:bold\"><td></td><td>Team Name</td><td>Module Name </td><td>Project Name</td><td>Team Description</td></tr>";
 
 		for (int i = 0; i < li.size(); i++) {
 			exp += "<tr><td><input type=\"radio\" name=\"teamedit\" value=\""+li.get(i).getTeamId()+"\"></td><td>" + li.get(i).getTeamName() + "</td>";
-			exp += "<td>" + li.get(i).getTeamId() + "</td>" + "<td>"
-					+ li.get(i).getModuleName() + "</td>" + "<td>"
+			exp += "<td>"+ li.get(i).getModuleName() + "</td>" + "<td>"
 					+ li.get(i).getProjectName() + "</td>"+ "<td>"
 							+ li.get(i).getTeamDescription() + "</td>";
 
@@ -62,5 +61,6 @@ public class FetchTeamDetails extends HttpServlet {
 		exp += "</table></div><input type=\"submit\" name=\"submit1\" value=\"Edit\"> <input type=\"submit\" name=\"submit2\" value=\"Delete\">";
 		
 		request.setAttribute("team", exp);
+		
 	}
 }
