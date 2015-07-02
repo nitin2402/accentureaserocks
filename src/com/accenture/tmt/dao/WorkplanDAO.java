6c1a7a6e967bb5bff212e1739bea9f2fbc6faeb8
nitin2402 <nitinsaini2402@gmail.com>
2015-07-02 11:20:44 +0530

    SB : WorkplanDAO

--- src/com/accenture/tmt/dao/WorkplanDAO.java
+++ src/com/accenture/tmt/dao/WorkplanDAO.java
@@ -3,6 +3,8 @@ package com.accenture.tmt.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.accenture.tmt.common.CONSTANTS;
 import com.accenture.tmt.common.DBConnection;
@@ -14,20 +16,19 @@ public class WorkplanDAO {
 		
 		int status_id = 1;
 		int flag = 0;
-		String teamId = workplanflatdto.getTeamId() ;
-		
 		try {
 			Connection con = DBConnection.getConnection();
 			if (con != null) {
 				PreparedStatement st = con
 						.prepareStatement(CONSTANTS.INSERT_REQUEST_QUERY);
 				st.setString(1, workplanflatdto.getReqId());
-				 st.setString(2, teamId);
+				 st.setString(2, workplanflatdto.getTeamId());
 				 st.setInt(3, workplanflatdto.getnASE());
 				 st.setInt(4, workplanflatdto.getnSE());
 				 st.setInt(5, workplanflatdto.getnSSE());
 				 st.setString(6, workplanflatdto.getComment());
 				 st.setInt(7, status_id);
+				 st.setString(8, workplanflatdto.getEmployeeId());
 				 
 				 flag = st.executeUpdate();
 				 
@@ -40,19 +41,18 @@ public class WorkplanDAO {
 		
 	}
 
-	public String fetchTeamId(String userName) {
+	public String fetchTeamId(String teamName) {
 		// TODO Auto-generated method stub
 		String teamId = null;
         ResultSet rs = null ;
-		
 		try {
 			Connection con = DBConnection.getConnection();
 			if (con != null) {
 				PreparedStatement st = con
 					  .prepareStatement(CONSTANTS.GET_TEAMID_QUERY);
-					  st.setString(1, userName);
-					  rs = st.executeQuery();
-					  while(rs.next()){
+				st.setString(1, teamName);
+				rs = st.executeQuery();
+				while(rs.next()){
 						  teamId = rs.getString("TeamId");
 				 }
 			}
@@ -61,6 +61,50 @@ public class WorkplanDAO {
 			e.printStackTrace();
 		}
 		return teamId;
+	}
+	
+	public String fetchEmployeeId(String userName) {
+		// TODO Auto-generated method stub
+		String employeeId = null;
+        ResultSet rs = null ;
+		try {
+			Connection con = DBConnection.getConnection();
+			if (con != null) {
+				PreparedStatement st = con
+					  .prepareStatement(CONSTANTS.GET_EMPLOYEEID_QUERY);
+				st.setString(1, userName);
+				rs = st.executeQuery();
+				while(rs.next()){
+						  employeeId = rs.getString("EmployeeId");
+				 }
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return employeeId;
+	}
+	
+	public List<String> getTeamList(){
+		
+		List<String> teamList = new ArrayList<String>();
+		ResultSet rs = null ;
+		try {
+			Connection con = DBConnection.getConnection();
+			if (con != null) {
+				PreparedStatement st = con
+					  .prepareStatement(CONSTANTS.GET_TEAM_LIST_QUERY);
+				rs = st.executeQuery();
+				while(rs.next()){
+						  teamList.add(rs.getString("TeamName"));
+				 }
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return teamList;
+		
 	}
 	
 	public int countRows(){
