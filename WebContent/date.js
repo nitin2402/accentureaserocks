4d611aafa926a60c18810cc97da2b10b44c945b2
nitin2402 <nitinsaini2402@gmail.com>
2015-07-06 17:50:11 +0530

    DB-added new datepicker

--- WebContent/date.js
+++ WebContent/date.js
@@ -1,8 +1,49 @@
 $(document).ready(function() {
-    $("#calendar").datepicker();
-    $("#calendar1").datepicker();
+    $("#calendar").datepicker({
+    	yearRange: '2000:2030',
+    	changeMonth: true,
+    	changeYear: true
+    });
+    $("#calendar1").datepicker({
+    	yearRange: '2000:2030',
+        changeMonth: true,
+        changeYear: true
+        });
     $("button").click(function() {
         var calendar = $("#calendar").val();
         var calendar1 = $("#calendar1").val();
     });
 });
+
+
+
+/*$('#validateForm').validate({ 
+    errorPlacement: $.datepicker.errorPlacement, 
+    rules: { 
+        validDefaultDatepicker: { 
+            required: true, 
+            dpDate: true 
+        }, 
+        validBeforeDatepicker: { 
+            dpCompareDate: ['before', '#calandar'] 
+        }, 
+        validAfterDatepicker: { 
+            dpCompareDate: {after: '#calandar1'} 
+        }
+    }, 
+    messages: { 
+         
+        validAfterDatepicker: 'Please enter a date after the date of joining' 
+    }});
+
+
+
+$(form).validate({ 
+    rules: { 
+        startDate: { 
+            required: true, 
+            dpDate: true 
+        } 
+    }     
+});*/
+
