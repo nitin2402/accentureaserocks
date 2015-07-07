9f8b1ac3bc3d7e50b4e6ba10c1916b6731c84e9b
nitin2402 <nitinsaini2402@gmail.com>
2015-07-07 15:45:35 +0530

    AddTeamManually - suchi

---
+++ src/com/accenture/tmt/presentation/servlet/TeamSelect.java
@@ -0,0 +1,46 @@
+package com.accenture.tmt.presentation.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddEmpManual
+ */
+public class AddTeamManually1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddTeamManually1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+String select = request.getParameter("select");
+		
+		if(select=="addfromexcel" || select .equals("addfromexcel"))
+			response.sendRedirect("store.jsp");
+		if(select=="addmanually" || select .equals("addmanually"))
+			response.sendRedirect("AddTeamManually.jsp");
+		
+	}
+
+}
