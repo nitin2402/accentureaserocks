3d3d3030cb23be82ea2ff1dc73c9689fd6d9dbcc
nitin2402 <nitinsaini2402@gmail.com>
2015-07-08 18:18:54 +0530

    RTM: DTO for module report

---
+++ src/com/accenture/tmt/presentation/dto/ModuleReportDTO.java
@@ -0,0 +1,36 @@
+package com.accenture.tmt.presentation.dto;
+
+import java.sql.Date;
+
+public class ModuleReportDTO {
+
+	
+	private String moduleName;
+	private Date startDate;
+	private Date endDate;
+	
+	public String getModuleName() {
+		return moduleName;
+	}
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+	
+}
