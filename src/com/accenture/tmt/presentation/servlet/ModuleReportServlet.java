f0c3b2f4e958f45c9dd6e7fdcb030a56c1a21fe6
nitin2402 <nitinsaini2402@gmail.com>
2015-07-08 18:16:07 +0530

    RTM: servlet for module report

---
+++ src/com/accenture/tmt/presentation/servlet/ModuleReportServlet.java
@@ -0,0 +1,186 @@
+package com.accenture.tmt.presentation.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.accenture.tmt.common.CONSTANTS;
+import com.accenture.tmt.dao.dto.ModuleReportFlatDTO;
+import com.accenture.tmt.manager.ModuleReportController;
+import com.accenture.tmt.presentation.dto.ModuleReportDTO;
+
+/**
+ * Servlet implementation class ModuleReportServlet
+ */
+public class ModuleReportServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModuleReportServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String moduleName = request.getParameter(CONSTANTS.GET_MODULE_NAME_FOR_REPORT);
+		   String startDate = request.getParameter(CONSTANTS.GET_START_DATE_FOR_MODULE_REPORT);
+		    String endDate = request.getParameter(CONSTANTS.GET_END_DATE_FOR_MODULE_REPORT);
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy"); 
+		// SimpleDateFormat df1 = new SimpleDateFormat("MM/dd/yyyy");
+		 Date startDate1 = null;
+	     
+		 Date endDate1 = null;
+		 java.sql.Date sqlStart=null;
+		 java.sql.Date sqlEnd = null;
+		 try {
+			startDate1 =   df.parse(startDate);
+			 sqlStart= new java.sql.Date(startDate1.getTime());
+		 } catch (ParseException e) {
+			 //TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+			 try {
+				endDate1 =  df.parse(endDate);
+				 sqlEnd= new java.sql.Date(endDate1.getTime());
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//String sd = df.format(startDate1);
+			//System.out.println(startDate1);
+			//System.out.println(df.format(startDate1)); 
+			
+			//String ed = df1.format(endDate1);
+		    // System.out.println(ed);
+			 //System.out.println(df.format(endDate1)); 
+		
+
+		
+		 
+		ModuleReportDTO modulereportdto = new ModuleReportDTO();
+		modulereportdto.setModuleName(moduleName);
+		modulereportdto.setStartDate(sqlStart);
+		modulereportdto.setEndDate(sqlEnd);
+		
+		if((moduleName == null || moduleName == "") && (startDate1 == null )){
+			ModuleReportController fetch = new ModuleReportController();
+			List<ModuleReportFlatDTO> modulereportlist = new ArrayList<ModuleReportFlatDTO>();
+			modulereportlist = fetch.reportModuleWithOnlyEndDate(modulereportdto);
+			request.setAttribute("ReportList", modulereportlist);
+			if(modulereportlist.isEmpty()){
+				response.sendRedirect("modulereportdetail.jsp?msg=Details are not found.");
+			}
+			else{
+			request.getRequestDispatcher("modulereportdetail.jsp").forward(request, response);
+			}
+		}
+		else if((startDate1 == null ) && (endDate1 == null )){
+			ModuleReportController fetch = new ModuleReportController();
+			List<ModuleReportFlatDTO> modulereportlist = new ArrayList<ModuleReportFlatDTO>();
+			modulereportlist = fetch.reportModuleWithOnlyModuleName(modulereportdto);
+			request.setAttribute("ReportList", modulereportlist);
+			
+			if(modulereportlist.isEmpty()){
+				response.sendRedirect("modulereportdetail.jsp?msg=Details are not found.");
+			}
+			else{
+			request.getRequestDispatcher("modulereportdetail.jsp").forward(request, response);
+			}
+		}
+		else if((moduleName == null || moduleName == "") && (endDate1 == null )){
+			ModuleReportController fetch = new ModuleReportController();
+			List<ModuleReportFlatDTO> modulereportlist = new ArrayList<ModuleReportFlatDTO>();
+			modulereportlist = fetch.reportModuleWithOnlyStartDate(modulereportdto);
+			request.setAttribute("ReportList", modulereportlist);
+			
+			if(modulereportlist.isEmpty()){
+				response.sendRedirect("modulereportdetail.jsp?msg=Details are not found.");
+			}
+			else{
+			request.getRequestDispatcher("modulereportdetail.jsp").forward(request, response);
+			}
+
+	}
+		else if(moduleName == null || moduleName == ""){	
+			ModuleReportController fetch = new ModuleReportController();
+			List<ModuleReportFlatDTO> modulereportlist = new ArrayList<ModuleReportFlatDTO>();
+			modulereportlist = fetch.reportModuleWithoutModuleName(modulereportdto);
+			request.setAttribute("ReportList", modulereportlist);
+			
+			if(modulereportlist.isEmpty()){
+				response.sendRedirect("modulereportdetail.jsp?msg=Details are not found.");
+			}
+			else{
+			request.getRequestDispatcher("modulereportdetail.jsp").forward(request, response);
+			}
+			}
+		else if(startDate1 == null ){
+			ModuleReportController fetch = new ModuleReportController();
+			List<ModuleReportFlatDTO> modulereportlist = new ArrayList<ModuleReportFlatDTO>();
+			modulereportlist = fetch.reportModuleWithoutStartDate(modulereportdto);
+			request.setAttribute("ReportList", modulereportlist);
+			
+			if(modulereportlist.isEmpty()){
+				response.sendRedirect("modulereportdetail.jsp?msg=Details are not found.");
+			}
+			else{
+			request.getRequestDispatcher("modulereportdetail.jsp").forward(request, response);
+			}
+			
+		}
+		else if(endDate1 == null){
+			ModuleReportController fetch = new ModuleReportController();
+			List<ModuleReportFlatDTO> modulereportlist = new ArrayList<ModuleReportFlatDTO>();
+			modulereportlist = fetch.reportModuleWithoutEndDate(modulereportdto);
+			request.setAttribute("ReportList", modulereportlist);
+			
+			if(modulereportlist.isEmpty()){
+				response.sendRedirect("modulereportdetail.jsp?msg=Details are not found.");
+			}
+			else{
+			request.getRequestDispatcher("modulereportdetail.jsp").forward(request, response);
+			}
+			
+			
+		}
+		else{
+			
+			ModuleReportController fetch = new ModuleReportController();
+			List<ModuleReportFlatDTO> modulereportlist = new ArrayList<ModuleReportFlatDTO>();
+			modulereportlist = fetch.reportModule(modulereportdto);
+			request.setAttribute("ReportList", modulereportlist);
+			
+			if(modulereportlist.isEmpty()){
+				response.sendRedirect("modulereportdetail.jsp?msg=Details are not found.");
+			}
+			else{
+			request.getRequestDispatcher("modulereportdetail.jsp").forward(request, response);
+			}
+		}
+
+}
+}
