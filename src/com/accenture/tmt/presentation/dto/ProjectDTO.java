1f0a290b4919a4a8df3ea663fbb1a8c2b4c37273
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 13:14:54 +0530

    pk: new DTO for project tab

---
+++ src/com/accenture/tmt/presentation/dto/ProjectDTO.java
@@ -0,0 +1,31 @@
+package com.accenture.tmt.presentation.dto;
+
+public class ProjectDTO {
+		private String ProjectName;
+		private String CurrentRelease;
+		private String ReleaseDate;
+		public String getProjectName() {
+			return ProjectName;
+		}
+		public void setProjectName(String projectName) {
+			ProjectName = projectName;
+		}
+		public String getCurrentRelease() {
+			return CurrentRelease;
+		}
+		public void setCurrentRelease(String currentRelease) {
+			CurrentRelease = currentRelease;
+		}
+		public String getReleaseDate() {
+			return ReleaseDate;
+		}
+		public void setReleaseDate(String releaseDate) {
+			ReleaseDate = releaseDate;
+		}
+		
+		
+
+	}
+
+
+
