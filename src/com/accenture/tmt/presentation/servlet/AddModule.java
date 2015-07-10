5def7ad2ff7284a0e3e1619d8a621b7852edcb18
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 11:41:46 +0530

    RTM: Changes on servlet for addition of a new module, module report gets updated.

--- src/com/accenture/tmt/presentation/servlet/AddModule.java
+++ src/com/accenture/tmt/presentation/servlet/AddModule.java
@@ -1,15 +1,24 @@
 package com.accenture.tmt.presentation.servlet;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
 
 import com.accenture.tmt.dao.dto.ModuleDetailsDTO;
+import com.accenture.tmt.dao.dto.ModuleReportFlatDTO;
 import com.accenture.tmt.manager.ModuleController;
+import com.accenture.tmt.manager.ModuleReportController;
 import com.accenture.tmt.presentation.dto.ModuleFormDTO;
+import com.accenture.tmt.presentation.dto.ModuleReportUpdateDTO;
 
 
 /**
@@ -42,8 +51,17 @@ public class AddModule extends HttpServlet {
 		
 		ModuleFormDTO moduleformdto = new ModuleFormDTO();
 		ModuleController modulecontroller = new ModuleController();
-		int status = 0;
-		
+		ModuleReportUpdateDTO reportupdatedto = new ModuleReportUpdateDTO();
+		ModuleReportController modulereportcontroller = new ModuleReportController();
+		ModuleDetailsDTO moduledetailsdto = new ModuleDetailsDTO();
+	
+		java.sql.Date sqlDate=null;
+	
+		HttpSession session1 = request.getSession();
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		Date date = new Date();
+		 String timestamp= df.format(date);
+		 sqlDate= new java.sql.Date(date.getTime());
 		String moduleName = request.getParameter("modulename");
 		String projectName = request.getParameter("project1");
 		String moduleDescription = request.getParameter("moduledesc");
@@ -54,16 +72,27 @@ public class AddModule extends HttpServlet {
 		moduleformdto.setModuleDescription(moduleDescription);
 		moduleformdto.setProject(projectName);
 		
-		status = modulecontroller.addModule1(moduleformdto);
-		
-		if(status == 1){
+		JSONObject obj = modulecontroller.addModule1(moduleformdto);
+		if(obj.get("status").toString().equals("1")){
 			request.setAttribute("message", "Module Inserted Successfully");
-		} else if(status == 0){
+			if(session1!= null){
+				reportupdatedto.setModuleId(moduleId);
+				reportupdatedto.setModuleName(moduleName);
+				reportupdatedto.setProjectId(obj.get("projectId").toString());
+				reportupdatedto.setModuleDescription(moduleDescription);
+				reportupdatedto.setUserName((String)session1.getAttribute("user"));
+				reportupdatedto.setAction("added");
+				reportupdatedto.setTimeStamp(timestamp);
+				reportupdatedto.setDates(sqlDate);
+			modulereportcontroller.updateModuleReport(reportupdatedto);
+			}
+			
+		} else if( obj.get("status").toString() == "0"){
 			request.setAttribute("message", "No Module Inserted	");
 		}
 		
 		request.getRequestDispatcher("admintool.jsp").forward(request, response);
 		
 	}
+	}
 
-}
