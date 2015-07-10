79065963f3b9aefd446593d36aee1fadbe8b1b16
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 12:15:15 +0530

    RTM: changes in servlet for module report updation when a module is edited.

--- src/com/accenture/tmt/presentation/servlet/EditModuleFinal.java
+++ src/com/accenture/tmt/presentation/servlet/EditModuleFinal.java
@@ -2,13 +2,18 @@ package com.accenture.tmt.presentation.servlet;
 
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.accenture.tmt.manager.ModuleController;
+import com.accenture.tmt.manager.ModuleReportController;
+import com.accenture.tmt.presentation.dto.ModuleReportUpdateDTO;
 
 
 
@@ -39,16 +44,36 @@ public class EditModuleFinal extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		java.sql.Date sqlDate=null;
 		String modulename = request.getParameter("modulename");
 		String project = request.getParameter("project");
 		String moduledesc = request.getParameter("moduledesc");
 		String moduleid = request.getParameter("moduleid");
 		
+		HttpSession session1 = request.getSession();
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		Date date = new Date();
+		 String timestamp= df.format(date);
+		 sqlDate= new java.sql.Date(date.getTime());
 		ModuleController edit = new ModuleController();
+		ModuleReportController modulereportcontroller = new ModuleReportController();
+		ModuleReportUpdateDTO reportupdatedto = new ModuleReportUpdateDTO();
 		int a = edit.editModule(modulename, project, moduledesc, moduleid);
 if(a!=0)
 {request.setAttribute("message","Module Updated");
+if(session1 != null){
+	reportupdatedto.setModuleId(moduleid);
+	reportupdatedto.setModuleName(modulename);
+	reportupdatedto.setProjectId(project);
+	reportupdatedto.setModuleDescription(moduledesc);
+	reportupdatedto.setUserName((String)session1.getAttribute("user"));
+	reportupdatedto.setAction("edited");
+	reportupdatedto.setTimeStamp(timestamp);
+	reportupdatedto.setDates(sqlDate);
+	modulereportcontroller.updateModuleReport(reportupdatedto);
 request.getRequestDispatcher("viewmodule.jsp").forward(request, response);
+}
 	}
 if (a==0)
 {
