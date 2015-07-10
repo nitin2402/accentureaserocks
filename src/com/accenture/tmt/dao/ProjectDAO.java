ef81850e0d98a17bf098f29309a4caa5936c5e1f
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 13:08:16 +0530

    pk: for project tab.

--- src/com/accenture/tmt/dao/ProjectDAO.java
+++ src/com/accenture/tmt/dao/ProjectDAO.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.accenture.tmt.common.CONSTANTS;
 import com.accenture.tmt.common.DBConnection;
+import com.accenture.tmt.presentation.dto.ProjectDTO;
 
 
 public class ProjectDAO {
@@ -64,7 +66,67 @@ private List<String> projectNames = new ArrayList<String>();
 		}
 
 		return projectNames;
-
-		
-	}
+	
+	public List<ProjectDTO> currentRelease(){
+			List<ProjectDTO> list = new ArrayList<ProjectDTO>();
+			try {
+				
+				Connection con = DBConnection.getConnection();
+				Statement st = con.createStatement();
+				
+				
+				ResultSet rs = st.executeQuery(CONSTANTS.Current);
+				
+				while(rs.next())
+				{
+					ProjectDTO current = new ProjectDTO();
+					current.setProjectName(rs.getString("ProjectName"));
+					current.setCurrentRelease(rs.getString("CurrentRelease"));
+					/*current.setReleaseDate(rs.getString("ReleaseDate"));*/
+					
+					list.add(current);
+				}
+				
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return list;
+		}
+	public List<ProjectDTO> releaseDate(){
+			List<ProjectDTO> list = new ArrayList<ProjectDTO>();
+			try {
+				
+				Connection con = DBConnection.getConnection();
+				PreparedStatement st = con.prepareStatement(CONSTANTS.Release);
+				
+				
+				ResultSet rs = st.executeQuery();
+				
+				while(rs.next())
+				{
+					ProjectDTO current = new ProjectDTO();
+					/*current.setProjectName(rs.getString("ProjectName"));*/
+					current.setCurrentRelease(rs.getString("CurrentRelease"));
+					current.setReleaseDate(rs.getString("ReleaseDate"));
+					System.out.println(current.getProjectName());
+					list.add(current);
+				}
+				
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return list;
+		}	
 }
