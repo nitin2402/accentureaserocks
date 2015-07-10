93ed10904f24302f360b6942c495e60e609d008a
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 12:24:54 +0530

    RTM: changes in servlet for module report updation when a module is added by excel

--- src/com/accenture/tmt/presentation/servlet/AddModuleExcel.java
+++ src/com/accenture/tmt/presentation/servlet/AddModuleExcel.java
@@ -2,13 +2,16 @@ package com.accenture.tmt.presentation.servlet;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -17,7 +20,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.accenture.tmt.common.CONSTANTS;
 import com.accenture.tmt.manager.ExcelController;
+import com.accenture.tmt.manager.ModuleReportController;
 import com.accenture.tmt.presentation.dto.ModuleFormDTO;
+import com.accenture.tmt.presentation.dto.ModuleReportUpdateDTO;
 
 /**
  * Servlet implementation class AddModuleExcel
@@ -60,7 +65,16 @@ public class AddModuleExcel extends HttpServlet {
 			List<ModuleFormDTO> listOfModule = new ArrayList<ModuleFormDTO>();
 			
 			ModuleFormDTO moduleFormDTO = null;
-		
+			HttpSession session1 = request.getSession();
+			ModuleReportUpdateDTO reportupdatedto = new ModuleReportUpdateDTO();
+			ModuleReportController modulereportcontroller = new ModuleReportController();
+			
+			java.sql.Date sqlDate=null;
+			
+			SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+			Date date = new Date();
+			 String timestamp= df.format(date);
+			 sqlDate= new java.sql.Date(date.getTime());
 				boolean headerRow = true;
 				for (Row rowOfModule : projectDetails) {
 					
@@ -102,6 +116,19 @@ public class AddModuleExcel extends HttpServlet {
 
 			if(c!=0){
 				request.setAttribute("message","Record Inserted");
+				if(session1!= null){
+					for(int i =0;i<listOfModule.size();i++){
+					reportupdatedto.setModuleId(listOfModule.get(i).getModuleId());
+					reportupdatedto.setModuleName(listOfModule.get(i).getModuleName());
+					reportupdatedto.setProjectId(listOfModule.get(i).getProjectId());
+					reportupdatedto.setModuleDescription(listOfModule.get(i).getModuleDescription());
+					reportupdatedto.setUserName((String)session1.getAttribute("user"));
+					reportupdatedto.setAction("added by excel");
+					reportupdatedto.setTimeStamp(timestamp);
+					reportupdatedto.setDates(sqlDate);
+				modulereportcontroller.updateModuleReport(reportupdatedto);
+				}
+				}
 				request.getRequestDispatcher("addmodulevia.jsp").forward(request, response);}
 			if(c == 0){
 				request.setAttribute("message","Record insertion failed");
