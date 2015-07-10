aa5c3366974c6bae3f16f7316d42c3353ac32be5
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 12:05:44 +0530

    RTM: DTO for module report updation .

---
+++ src/com/accenture/tmt/presentation/dto/ModuleReportUpdateDTO.java
@@ -0,0 +1,66 @@
+package com.accenture.tmt.presentation.dto;
+
+import java.sql.Date;
+
+public class ModuleReportUpdateDTO {
+	private String moduleId;
+	private String moduleName;
+	private String projectId;
+	private String moduleDescription;
+	private String userName;
+	private String action;
+	private String timeStamp;
+	private Date Dates;
+	public String getModuleId() {
+		return moduleId;
+	}
+	public void setModuleId(String moduleId) {
+		this.moduleId = moduleId;
+	}
+	public String getModuleName() {
+		return moduleName;
+	}
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+	public String getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+	public String getModuleDescription() {
+		return moduleDescription;
+	}
+	public void setModuleDescription(String moduleDescription) {
+		this.moduleDescription = moduleDescription;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public Date getDates() {
+		return Dates;
+	}
+	public void setDates(Date dates) {
+		Dates = dates;
+	}
+	
+	
+
+}
