38da7298b6294d7a920b505747fa9641db26db00
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 19:55:53 +0530

    SB : changes done

--- src/com/accenture/tmt/manager/WorkplanAdminController.java
+++ src/com/accenture/tmt/manager/WorkplanAdminController.java
@@ -1,7 +1,5 @@
 package com.accenture.tmt.manager;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.accenture.tmt.dao.WorkplanAdminDAO;
@@ -13,79 +11,36 @@ public class WorkplanAdminController {
 	
 	public WorkplanAdminDTO fetchCalculationDetails(ActionDTO actiondto){
 		
-		int aseRequestedValue = 0;
-		int seRequestedValue = 0;
-		int sseRequestedValue = 0;
-		int totalRequestedValue = 0;
-		String aseCurrency = null;
-		String seCurrency = null;
-		String sseCurrency = null;
-		String tlCurrency = null;
-		int currentASEValue = 0;
-		int currentSEValue = 0;
-		int currentSSEValue = 0;
-		int currentTLValue = 0;
-		int totalCurrentValue = 0;
-		Map<String, List<String>> details = new HashMap<String, List<String>>();
+		int aseRequested = 0;
+		int seRequested = 0;
+		int sseRequested = 0;
+		Map<String, Integer> freeASEList ;
+		Map<String, Integer> freeSEList ;
+		Map<String, Integer> freeSSEList ;
+		float currentAvg = 0;
 		
 		WorkplanAdminDAO workplanAdminDAO = new WorkplanAdminDAO();
 		WorkplanAdminFlatDTO workplanAdminFlatDTO = workplanAdminDAO.fetchData(actiondto);
 		WorkplanAdminDTO workplanAdminDTO = new WorkplanAdminDTO();
 		
-		details = workplanAdminFlatDTO.getDetails();
+		freeASEList = workplanAdminFlatDTO.getFreeASEList();
+		freeSEList = workplanAdminFlatDTO.getFreeSEList();
+		freeSSEList = workplanAdminFlatDTO.getFreeSSEList();
+		currentAvg = workplanAdminFlatDTO.getCurrentAvg();
+		aseRequested = workplanAdminFlatDTO.getAseRequested();
+		seRequested = workplanAdminFlatDTO.getSeRequested();
+		sseRequested = workplanAdminFlatDTO.getSseRequested();
 		
-		workplanAdminDTO.setAseRequested(workplanAdminFlatDTO.getAseRequested());
-		workplanAdminDTO.setSeRequested(workplanAdminFlatDTO.getSeRequested());
-		workplanAdminDTO.setSseRequested(workplanAdminFlatDTO.getSseRequested());
-		workplanAdminDTO.setCurrentASE(workplanAdminFlatDTO.getCurrentASE());
-		workplanAdminDTO.setCurrentSE(workplanAdminFlatDTO.getCurrentSE());
-		workplanAdminDTO.setCurrentSSE(workplanAdminFlatDTO.getCurrentSSE());
-		workplanAdminDTO.setCurrentTL(workplanAdminFlatDTO.getCurrentTL());
-		workplanAdminDTO.setDetails(details);
-		
-		List<String> value;
-		value = details.get("ASE");
-		aseRequestedValue = (Integer.parseInt(value.get(1))) * (workplanAdminFlatDTO.getAseRequested()) ;
-		currentASEValue = (Integer.parseInt(value.get(1))) * (workplanAdminFlatDTO.getCurrentASE()) ;
-		aseCurrency = value.get(0);
-		
-		value = details.get("SE");
-		seRequestedValue = (Integer.parseInt(value.get(1))) * (workplanAdminFlatDTO.getSeRequested()) ;
-		currentSEValue = (Integer.parseInt(value.get(1))) * (workplanAdminFlatDTO.getCurrentSE()) ;
-		seCurrency = value.get(0);
-		
-		value = details.get("SSE");
-		sseRequestedValue = (Integer.parseInt(value.get(1))) * (workplanAdminFlatDTO.getSeRequested()) ;
-		currentSSEValue = (Integer.parseInt(value.get(1))) * (workplanAdminFlatDTO.getCurrentSSE()) ;
-		sseCurrency = value.get(0);
-		
-		value = details.get("TL");
-		currentTLValue = (Integer.parseInt(value.get(1))) * (workplanAdminFlatDTO.getCurrentTL()) ;
-		tlCurrency = value.get(0);
-		
-		totalRequestedValue = aseRequestedValue + seRequestedValue + sseRequestedValue ;
-		totalCurrentValue = currentASEValue + currentSEValue + currentSSEValue + currentTLValue;
-		
-		workplanAdminDTO.setAseRequestedValue(aseRequestedValue);
-		workplanAdminDTO.setSeRequestedValue(seRequestedValue);
-		workplanAdminDTO.setSseRequestedValue(sseRequestedValue);
-		workplanAdminDTO.setTotalRequestedValue(totalRequestedValue);
-		workplanAdminDTO.setAseCurrency(aseCurrency);
-		workplanAdminDTO.setSeCurrency(seCurrency);
-		workplanAdminDTO.setSseCurrency(sseCurrency);
-		workplanAdminDTO.setTlCurrency(tlCurrency);
-		workplanAdminDTO.setCurrentASEValue(currentASEValue);
-		workplanAdminDTO.setCurrentSEValue(currentSEValue);
-		workplanAdminDTO.setCurrentSSEValue(currentSSEValue);
-		workplanAdminDTO.setCurrentTLValue(currentTLValue);
-		workplanAdminDTO.setTotalCurrentValue(totalCurrentValue);
+		workplanAdminDTO.setAseRequested(aseRequested);
+		workplanAdminDTO.setSeRequested(seRequested);
+		workplanAdminDTO.setSseRequested(sseRequested);
+		workplanAdminDTO.setFreeASEList(freeASEList);
+		workplanAdminDTO.setFreeSEList(freeSEList);
+		workplanAdminDTO.setFreeSSEList(freeSSEList);
+		workplanAdminDTO.setCurrentAvg(currentAvg);
 		
 		return workplanAdminDTO;
 		}
-		
-		
-		
-		
 		
 	}
 
