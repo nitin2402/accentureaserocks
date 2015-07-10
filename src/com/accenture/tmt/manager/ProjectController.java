fca1d099ea18e5fc602239dbb5b525006e26af4b
nitin2402 <nitinsaini2402@gmail.com>
2015-07-10 13:13:23 +0530

    pk: for project tab

--- src/com/accenture/tmt/manager/ProjectController.java
+++ src/com/accenture/tmt/manager/ProjectController.java
@@ -1,8 +1,10 @@
 package com.accenture.tmt.manager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.accenture.tmt.dao.ProjectDAO;
+import com.accenture.tmt.presentation.dto.ProjectDTO;
 
 public class ProjectController {
 	
@@ -13,5 +15,15 @@ public class ProjectController {
 		return projectList;
 		
 	}
-
+	public List<ProjectDTO> currentRelease(){
+		List<ProjectDTO> list= new ArrayList<ProjectDTO>();
+		list = projectDAO.currentRelease();
+		return list;
+		
+	}
+	public List<ProjectDTO> releaseDate(){
+		List<ProjectDTO> list= new ArrayList<ProjectDTO>();
+		list = projectDAO.releaseDate();
+		return list;
+	}
 }
