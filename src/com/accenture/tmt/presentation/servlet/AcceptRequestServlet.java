7e6e7282160cd19ca4c797cf27cbc90d997c539e
nitin2402 <nitinsaini2402@gmail.com>
2015-07-13 18:11:17 +0530

    SB : added AcceptRequestServlet

---
+++ src/com/accenture/tmt/presentation/servlet/AcceptRequestServlet.java
@@ -0,0 +1,69 @@
+package com.accenture.tmt.presentation.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.accenture.tmt.manager.ActionController;
+import com.accenture.tmt.presentation.dto.ActionDTO;
+
+/**
+ * Servlet implementation class AcceptRequestServlet
+ */
+public class AcceptRequestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AcceptRequestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		int flag = 0;
+		
+		ActionController controller = new ActionController();
+		ActionDTO actiondto = new ActionDTO();
+		
+		String [] empASE = request.getParameterValues("empASE[]");
+		String [] empSE = request.getParameterValues("empSE[]");
+		String [] empSSE = request.getParameterValues("empSSE[]");
+		String reqId = request.getParameter("reqId");
+		String reason = request.getParameter("reason");
+		
+		actiondto.setReqId(reqId);
+		actiondto.setReason(reason);
+
+		flag = controller.acceptRequest(empASE, empSE, empSSE, actiondto);
+		
+		if(flag == 1){
+			request.setAttribute("reqId", reqId);
+			request.setAttribute("result", "Accepted");
+			request.getRequestDispatcher("searchrequest.jsp").forward(request, response);
+			} else {
+				request.setAttribute("reqId", reqId);
+				request.setAttribute("result", "Not Accepted");
+				request.getRequestDispatcher("searchrequest.jsp").forward(request, response);
+			}
+		
+	}
+
+}
