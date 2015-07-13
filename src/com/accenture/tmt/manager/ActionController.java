c0a37fb0d8ca3b834f33c97f38f292209fdbe660
nitin2402 <nitinsaini2402@gmail.com>
2015-07-13 18:02:35 +0530

    SB : modified acceptRequest method

--- src/com/accenture/tmt/manager/ActionController.java
+++ src/com/accenture/tmt/manager/ActionController.java
@@ -9,13 +9,46 @@ public class ActionController {
 	ActionDAO actiondao = new ActionDAO();
 	ActionFlatDTO actionflatdto = new ActionFlatDTO();
 	int flag = 0;
-	public int acceptRequest(ActionDTO actiondto) {
+	
+	public int acceptRequest(String[] empASE, String[] empSE, String[] empSSE, 
+			ActionDTO actiondto){
+		
+		String[] temp = new String[2];
+		String[] empNamesASE = null ;
+		String[] empNamesSE = null ;
+		String[] empNamesSSE = null ;
+		
+		if(empASE != null){
+			int aseLength = empASE.length ;
+			empNamesASE = new String[aseLength] ;
+			for(int i=0; i < aseLength; i++){
+				temp = empASE[i].split("_") ;
+				empNamesASE[i] = temp[1] ;
+			}
+		}
+		
+		if(empSE != null){
+		int seLength = empSE.length ;
+		empNamesSE = new String[seLength] ;
+		for(int i=0; i < seLength; i++){
+			temp = empSE[i].split("_") ;
+			empNamesSE[i] = temp[1] ;
+		}
+		}
+		
+		if(empSSE != null){
+			int sseLength = empSSE.length ;
+			empNamesSSE = new String[sseLength] ;
+			for(int i=0; i < sseLength; i++){
+				temp = empSSE[i].split("_") ;
+				empNamesSSE[i] = temp[1] ;
+			}
+		}
 		
 		actionflatdto.setReqId(actiondto.getReqId());
 		actionflatdto.setReason(actiondto.getReason());
-		flag = actiondao.accept(actionflatdto);
-		return flag;
-		
+		flag = actiondao.accept(empNamesASE, empNamesSE, empNamesSSE, actionflatdto);
+		return flag ;
 	}
 
 	public int rejectRequest(ActionDTO actiondto) {
