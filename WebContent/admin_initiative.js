e008836739e0c014ea64010481900965ec70f2f7
nitin2402 <nitinsaini2402@gmail.com>
2015-07-15 15:26:47 +0530

    SB : added

---
+++ WebContent/admin_initiative.js
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+ 
+ var extension = ".pdf";    
+	function Validate(vaildateObj) {
+		
+		var dataFile=document.forms["upload"]["dataFile"].value;
+		if (dataFile == null || dataFile == ""){
+			alert("Please select a file");
+			return false;
+		}
+		
+	    var getFileName = vaildateObj.getElementsByTagName("input");
+	  
+	        var fileInput = getFileName[0];
+	        if (fileInput.type == "file"){
+	         var fileName = fileInput.value;
+	        
+	            if (fileName.length > 0) {
+	                var isValid = false;
+	                 
+	                    if (fileName.substr(fileName.length - extension.length, extension.length).toLowerCase() == extension.toLowerCase()) {
+	                        isValid = true;
+	                    }
+	                 
+	                if (!isValid) {
+	                    alert("Only " + extension + " extension can be uploaded " );
+	                    return false;
+	                }
+	                
+	            }
+	        }
+	        
+	         var size = $("#fileUpload")[0].files[0].size ;
+
+	         if (size > 1048576){
+	        	 alert("File size should be less than 1 MB");
+	        	 return false ;
+	         }
+	}
+	            
+	    
+  
+ 
+
+
+
+
+
