8b6f0eb7122976d2914ca01f2305c6c1c9c25a79
nitin2402 <nitinsaini2402@gmail.com>
2015-07-15 18:33:20 +0530

    RTM: changes in dto for authorize employee

--- src/com/accenture/tmt/presentation/dto/LoginDTO.java
+++ src/com/accenture/tmt/presentation/dto/LoginDTO.java
@@ -1,10 +1,21 @@
 package com.accenture.tmt.presentation.dto;
 
 public class LoginDTO {
+	private String loginid;
 private String username;
 private String password;
 private String userType;
+private String employeeId;
+private String activeUser;
+private String employeedesignation;
 
+
+public String getLoginid() {
+	return loginid;
+}
+public void setLoginid(String loginid) {
+	this.loginid = loginid;
+}
 public String getUserType() {
 	return userType;
 }
@@ -23,5 +34,24 @@ public String getPassword() {
 public void setPassword(String password) {
 	this.password = password;
 }
+public String getEmployeeId() {
+	return employeeId;
+}
+public void setEmployeeId(String employeeId) {
+	this.employeeId = employeeId;
+}
+public String getActiveUser() {
+	return activeUser;
+}
+public void setActiveUser(String activeUser) {
+	this.activeUser = activeUser;
+}
+public String getEmployeedesignation() {
+	return employeedesignation;
+}
+public void setEmployeedesignation(String employeedesignation) {
+	this.employeedesignation = employeedesignation;
+}
+
 
 }
