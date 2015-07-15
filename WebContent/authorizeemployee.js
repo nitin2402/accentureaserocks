da9bcd5fe029898700921707c534da3072990332
nitin2402 <nitinsaini2402@gmail.com>
2015-07-15 18:19:02 +0530

    RTM: js for authorize employee

---
+++ WebContent/authorizeemployee.js
@@ -0,0 +1,86 @@
+function validateAdmin(){
+	
+	/*var id = $('#select').val();*/
+	 if ($("input[name=select]").is(":checked")) {
+		 
+		var id = $("input[name=select]:checked").val();
+	   var desig = $('#'+id).val();
+ 
+	if(desig == 'ASE' || desig == 'SE' || desig == 'SSE' || desig == 'TL'){
+	    var answer = confirm('Designation Mismatch !!! Do you want to approve?');
+		if (answer)
+		{
+		  return true;
+		}
+		else
+		{
+			  return false;
+		}
+		 
+	}
+	else if(desig == 'AM' || desig == 'M' || desig == 'SM' || desig == 'Accenture Leadership'){
+		return true;
+	}
+	 }else{
+		   alert("nothing is checked");
+		   return false;
+		 }
+}
+	
+	
+
+
+function validateUser(){
+	if ($("input[name=select]").is(":checked")) {
+	var id = $("input[name=select]:checked").val();
+	var desig = $('#'+id).val();
+	
+	if(desig == 'ASE' || desig == 'SE' || desig == 'SSE' ){
+	    var answer = confirm('Designation Mismatch !!! Do you want to approve?');
+		if (answer)
+		{
+		  return true;
+		}
+		else
+		{
+			  return false;
+		}
+		 
+	}
+	else if( desig == 'TL'|| desig == 'AM' || desig == 'M' || desig == 'SM' || desig == 'Accenture Leadership'){
+		return true;
+	}
+	}else{
+		   alert("nothing is checked");
+		   return false;
+		 }
+	
+}
+
+function validateViewer(){
+	if ($("input[name=select]").is(":checked")) {
+	var id = $("input[name=select]:checked").val();
+	var desig = $('#'+id).val();
+	 if( desig == 'TL'|| desig == 'AM' || desig == 'M' || desig == 'SM' || desig == 'Accenture Leadership'){
+		var answer = confirm('Designation Mismatch !!! Do you want to approve?');
+		if (answer)
+		{
+		  return true;
+		}
+		else
+		{
+	      return false;
+		}
+	 }
+		else if(desig == 'ASE' || desig == 'SE' || desig == 'SSE' ){
+		    return true;
+		}
+	}else{
+			   alert("nothing is checked");
+			   return false;
+			 }
+		 
+	
+}
+	
+
