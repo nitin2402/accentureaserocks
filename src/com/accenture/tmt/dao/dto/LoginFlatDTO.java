3128aaf5d1861dba041326f6835607ccd61dbbaa
nitin2402 <nitinsaini2402@gmail.com>
2015-07-15 18:35:25 +0530

    RTM: flatDTO for authorise employee

--- src/com/accenture/tmt/dao/dto/LoginFlatDTO.java
+++ src/com/accenture/tmt/dao/dto/LoginFlatDTO.java
@@ -1,9 +1,20 @@
 package com.accenture.tmt.dao.dto;
 
 public class LoginFlatDTO {
+	private String LoginId;
 	private String userName;
 	private String passWord;
 	private String typeOfUser;
+	private String employeeId;
+	private String activeUser;
+	private String employeedesignation;
+	
+	public String getLoginId() {
+		return LoginId;
+	}
+	public void setLoginId(String loginId) {
+		LoginId = loginId;
+	}
 	public String getUserName() {
 		return userName;
 	}
@@ -22,4 +33,24 @@ public class LoginFlatDTO {
 	public void setTypeOfUser(String typeOfUser) {
 		this.typeOfUser = typeOfUser;
 	}
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getActiveUser() {
+		return activeUser;
+	}
+	public void setActiveUser(String activeUser) {
+		this.activeUser = activeUser;
+	}
+	public String getEmployeedesignation() {
+		return employeedesignation;
+	}
+	public void setEmployeedesignation(String employeedesignation) {
+		this.employeedesignation = employeedesignation;
+	}
+	
+	
 }
