488c9422f48462b0befc250c8597775e10e2800c
nitin2402 <nitinsaini2402@gmail.com>
2015-07-16 12:38:19 +0530

    piechart

---
+++ src/com/accenture/tmt/dao/dto/pieCount.java
@@ -0,0 +1,154 @@
+package com.accenture.tmt.dao.dto;
+
+public class pieCount {
+	int count = 0;
+	int billable=0;
+	int ASE=0;
+	int SE=0;
+	int SSE=0;
+	int TL=0;
+	int AM=0;
+	int angularJS=0;
+	int AT=0;
+	int C_UNIX=0;
+	int FST=0;
+	int HTML=0;
+	int java=0;
+	int JSP=0;
+	int QA=0;
+	int TBD=0;
+	int p0=0;
+	int p1=0;
+	int p2=0;
+	int p3=0;
+	int p4=0;
+	
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getBillable() {
+		return billable;
+	}
+	public void setBillable(int billable) {
+		this.billable = billable;
+	}
+	public int getASE() {
+		return ASE;
+	}
+	public void setASE(int aSE) {
+		ASE = aSE;
+	}
+	public int getSE() {
+		return SE;
+	}
+	public void setSE(int sE) {
+		SE = sE;
+	}
+	public int getSSE() {
+		return SSE;
+	}
+	public void setSSE(int sSE) {
+		SSE = sSE;
+	}
+	public int getTL() {
+		return TL;
+	}
+	public void setTL(int tL) {
+		TL = tL;
+	}
+	public int getAM() {
+		return AM;
+	}
+	public void setAM(int aM) {
+		AM = aM;
+	}
+	public int getAngularJS() {
+		return angularJS;
+	}
+	public void setAngularJS(int angularJS) {
+		this.angularJS = angularJS;
+	}
+	public int getAT() {
+		return AT;
+	}
+	public void setAT(int aT) {
+		AT = aT;
+	}
+	public int getC_UNIX() {
+		return C_UNIX;
+	}
+	public void setC_UNIX(int c_UNIX) {
+		C_UNIX = c_UNIX;
+	}
+	public int getFST() {
+		return FST;
+	}
+	public void setFST(int fST) {
+		FST = fST;
+	}
+	public int getHTML() {
+		return HTML;
+	}
+	public void setHTML(int hTML) {
+		HTML = hTML;
+	}
+	public int getJava() {
+		return java;
+	}
+	public void setJava(int java) {
+		this.java = java;
+	}
+	public int getJSP() {
+		return JSP;
+	}
+	public void setJSP(int jSP) {
+		JSP = jSP;
+	}
+	public int getQA() {
+		return QA;
+	}
+	public void setQA(int qA) {
+		QA = qA;
+	}
+	public int getTBD() {
+		return TBD;
+	}
+	public void setTBD(int tBD) {
+		TBD = tBD;
+	}
+	public int getP0() {
+		return p0;
+	}
+	public void setP0(int p0) {
+		this.p0 = p0;
+	}
+	public int getP1() {
+		return p1;
+	}
+	public void setP1(int p1) {
+		this.p1 = p1;
+	}
+	public int getP2() {
+		return p2;
+	}
+	public void setP2(int p2) {
+		this.p2 = p2;
+	}
+	public int getP3() {
+		return p3;
+	}
+	public void setP3(int p3) {
+		this.p3 = p3;
+	}
+	public int getP4() {
+		return p4;
+	}
+	public void setP4(int p4) {
+		this.p4 = p4;
+	}
+
+
+}
