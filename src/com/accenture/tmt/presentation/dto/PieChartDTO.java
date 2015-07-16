3c1957dfc80d6640968e08c1cae3591b42f300ce
nitin2402 <nitinsaini2402@gmail.com>
2015-07-16 13:35:12 +0530

    PIECHARTDTO

---
+++ src/com/accenture/tmt/presentation/dto/PieChartDTO.java
@@ -0,0 +1,13 @@
+package com.accenture.tmt.presentation.dto;
+
+public class PieChartDTO {
+private String project;
+
+public String getProject() {
+	return project;
+}
+
+public void setProject(String project) {
+	this.project = project;
+}
+}
