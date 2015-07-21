f972ea5a05a8360a72189496d9c8c8610c27063f
nitin2402 <nitinsaini2402@gmail.com>
2015-07-21 16:13:57 +0530

    GK: changes in addmodule.js

--- WebContent/addmodule.js
+++ WebContent/addmodule.js
@@ -1,14 +1,10 @@
 function validateform1() {
 
-		var moduleid = document.forms["add"]["module_id"].value;
+		
 		var modulename = document.forms["add"]["module_name"].value;
 		var moduledesc = document.forms["add"]["module_desc"].value;
 		
 
-		if (moduleid == null || moduleid == "") {
-			alert("Please provide MODULE ID");
-			return false;
-		}
 		if (modulename == null || modulename == "") {
 			alert("Please provide MODULE NAME");
 			return false;
