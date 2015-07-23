2db41e4c3cd91099a815ac32899c4afba3d845b5
nitin2402 <nitinsaini2402@gmail.com>
2015-07-23 19:48:10 +0530

    VVM : TeamRportFlatDTO is completed.

---
+++ src/com/accenture/tmt/dao/dto/TeamReportFlatDTO.java
@@ -0,0 +1,64 @@
+package com.accenture.tmt.dao.dto;
+
+import java.sql.Date;
+
+public class TeamReportFlatDTO {
+	private String teamName;
+	private String teamId;
+	private String moduleId;
+	private String teamDescription;
+	private String username;
+	private String action;
+	private String timestamp;
+	private Date date;
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getTeamId() {
+		return teamId;
+	}
+	public void setTeamId(String teamId) {
+		this.teamId = teamId;
+	}
+	public String getModuleId() {
+		return moduleId;
+	}
+	public void setModuleId(String moduleId) {
+		this.moduleId = moduleId;
+	}
+	public String getTeamDescription() {
+		return teamDescription;
+	}
+	public void setTeamDescription(String teamDescription) {
+		this.teamDescription = teamDescription;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
