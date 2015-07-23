4665544c7b0a908e1bb74a459530e0c48b9c45a2
nitin2402 <nitinsaini2402@gmail.com>
2015-07-23 19:34:40 +0530

    VVM : teamreport.js has been made and committed.

---
+++ WebContent/teamreport.js
@@ -0,0 +1,15 @@
+function validateform(){
+	var teamName = document.forms["teamreport"]["teamName"].value;
+	var startDate = document.forms["teamreport"]["startDate"].value;
+	var endDate = document.forms["teamreport"]["endDate"].value;
+	if((teamName == null || teamName == "") && (startDate == null || startDate == "") && (endDate == null || endDate == "")){
+		alert("Fields cannot be empty");
+		return false;
+	}
+	
+	if ((Date.parse(startDate) > Date.parse(endDate))) {
+        alert("End date should be greater than Start date");
+        return false;
+    }
+	
+}
