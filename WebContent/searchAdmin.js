2dc2f621d264c171f85ff98e910c29d32bc3a97f
nitin2402 <nitinsaini2402@gmail.com>
2015-07-23 18:09:15 +0530

    pk: edit & delete employee

---
+++ WebContent/searchAdmin.js
@@ -0,0 +1,42 @@
+
+	$(document).ready(
+			function(e) {
+
+				$('#search_field').keyup(
+						function(event) {
+							var searchvalue = $("#search_field").val();
+							$(".error").hide();
+							$.ajax({
+								url : "/TMT_Test/SearchEmployeeAdmin",
+								type : "POST",
+								data : 'searchName=' + searchvalue,
+
+								success : function(data) {
+
+									$(".header_01").hide();
+									$(".header_09").show();
+									$(".header_09").text(
+											'Search results for \"'
+													+ searchvalue + '\"');
+							if (data != "") {
+										$("#search").hide();
+										 $("#ajaxResponse").show();
+										$("#ajaxResponse").html(data); 
+									} else {
+
+										 $("#ajaxResponse").hide(); 
+
+										$("#search").show().delay(1000);
+										$(".header_09").hide();
+										$(".header_01").show();
+
+									} 
+
+								},
+								error : function() {
+
+								}
+							});
+
+						});
+			});
