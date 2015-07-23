d29d6a13e415b9184c8223cb93d8421d14aeee1b
nitin2402 <nitinsaini2402@gmail.com>
2015-07-23 19:49:35 +0530

    VVM : TeamReportDTO is completed and committed.

---
+++ src/com/accenture/tmt/presentation/dto/TeamReportDTO.java
@@ -0,0 +1,32 @@
+package com.accenture.tmt.presentation.dto;
+
+import java.sql.Date;
+
+public class TeamReportDTO {
+
+
+	private String teamName;
+	private Date startDate;
+	private Date endDate;
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+}
