134283f395dfaba3466627c50a4b68bf939a8449
nitin2402 <nitinsaini2402@gmail.com>
2015-07-27 15:27:48 +0530

    Dynamic Pie chart

--- src/com/accenture/tmt/presentation/servlet/GetPieChartValues.java
+++ src/com/accenture/tmt/presentation/servlet/GetPieChartValues.java
@@ -1,6 +1,7 @@
 package com.accenture.tmt.presentation.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +9,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
 
 import com.accenture.tmt.dao.dto.pieCount;
 import com.accenture.tmt.manager.EmployeeController;
@@ -41,7 +44,8 @@ public class GetPieChartValues extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       
-		String project = request.getParameter("");
+		String project = request.getParameter("projectId");
+		System.out.println(project);
 		PieChartDTO pieDTO = new PieChartDTO();
 		pieDTO.setProject(project);
 		EmployeeController getEmpDetails = new EmployeeController();
@@ -49,7 +53,35 @@ public class GetPieChartValues extends HttpServlet {
 		String pielist=null;
 		piechartlist=getEmpDetails.fetchPieDetails(pieDTO);
 		
-		request.setAttribute("pielist", piechartlist);
+	
+		PrintWriter out = response.getWriter();
+		JSONObject json = new JSONObject();
+		json.put("ASE",piechartlist.get(0).getASE());
+		json.put("SE",piechartlist.get(0).getSE());
+		json.put("SSE",piechartlist.get(0).getSSE());
+		json.put("TL",piechartlist.get(0).getTL());
+		json.put("AM",piechartlist.get(0).getAM());
+		json.put("M",piechartlist.get(0).getM());
+		json.put("total",piechartlist.get(0).getCount());
+		json.put("billable",piechartlist.get(0).getBillable());
+		
+		json.put("p0",piechartlist.get(0).getP0());
+		json.put("p1",piechartlist.get(0).getP1());
+		json.put("p2",piechartlist.get(0).getP2());
+		json.put("p3",piechartlist.get(0).getP3());
+		json.put("p4",piechartlist.get(0).getP4());
+		
+		json.put("angularJS",piechartlist.get(0).getAngularJS());
+		
+		json.put("AT",piechartlist.get(0).getAT());
+		json.put("C_UNIX",piechartlist.get(0).getC_UNIX());
+		json.put("FST",piechartlist.get(0).getFST());
+		json.put("HTML",piechartlist.get(0).getHTML());
+		json.put("java",piechartlist.get(0).getJava());
+		json.put("JSP",piechartlist.get(0).getJSP());
+		json.put("QA",piechartlist.get(0).getQA());
+		json.put("name", piechartlist.get(0).getName());
+		out.print(json);
 		//request.getRequestDispatcher("adminhome.jsp").forward(request, response);
 		
 	}
