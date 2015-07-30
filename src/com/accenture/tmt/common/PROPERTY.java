9913541b29bf7121f3e50c098acdc1257d331767
nitin2402 <nitinsaini2402@gmail.com>
2015-07-30 15:16:08 +0530

    sd-for excel

---
+++ src/com/accenture/tmt/common/PROPERTY.java
@@ -0,0 +1,6 @@
+package com.accenture.tmt.common;
+
+public interface PROPERTY {
+	public String FETCHDETAILS="SELECT Team.TeamId FROM ProjectDetail INNER JOIN ModuleDetail ON ProjectDetail.ProjectId=ModuleDetail.ProjectId INNER JOIN Team ON Team.ModuleId=ModuleDetail.ModuleId where ProjectDetail.ProjectName=? AND ModuleDetail.ModuleName=? AND Team.TeamName=?";
+
+}
