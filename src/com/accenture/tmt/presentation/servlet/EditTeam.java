ce9a32c07bc6658b924bc50adc65ef523b8ced3a
nitin2402 <nitinsaini2402@gmail.com>
2015-08-04 13:14:45 +0530

    VVM : TeamReport part has been added in this and completed.

--- src/com/accenture/tmt/presentation/servlet/EditTeam.java
+++ src/com/accenture/tmt/presentation/servlet/EditTeam.java
@@ -3,17 +3,22 @@ package com.accenture.tmt.presentation.servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.accenture.tmt.dao.dto.TeamDetailsDTO;
 import com.accenture.tmt.dao.dto.TeamDetailsFlatDTO;
 import com.accenture.tmt.manager.TeamController;
+import com.accenture.tmt.manager.TeamReportController;
+import com.accenture.tmt.presentation.dto.TeamReportUpdateDTO;
 
 
 
@@ -44,6 +49,18 @@ public class EditTeam extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		TeamReportUpdateDTO reportUpdateDTO = new TeamReportUpdateDTO();
+		TeamReportController reportController = new TeamReportController();
+		HttpSession session1 = request.getSession();
+		
+		java.sql.Date sqlDate=null;
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		Date date = new Date();
+		String timestamp= df.format(date);
+		sqlDate= new java.sql.Date(date.getTime());
+		
+		
 		String submit1=request.getParameter("submit1");
 		String submit2=request.getParameter("submit2");
 		String team = request.getParameter("teamedit");
@@ -69,11 +86,8 @@ public class EditTeam extends HttpServlet {
 		/*ModuleManager m = new ModuleManager();
 		String mid = m.fetchModuleid(module);*/
 	
+		
 		/*String tid = editteam.fetchTeamid(team,mid);
-	
-		
-		
-	
 		
 		int a =editteam.EditTeam(n_team, mid,tid);
 		if(a>0)
@@ -86,13 +100,16 @@ response.sendRedirect("editteam.jsp?msg1=Module Edited");
 				response.sendRedirect("editteam.jsp?msg1=Failed to save changes !!");
 						
 		}*/
+		
 		request.setAttribute("team", li);
 		request.getRequestDispatcher("editteam.jsp").forward(request, response);
 	}
 	
 	if(submit2!=null){
 		
-	
+		List<TeamDetailsFlatDTO> li = new ArrayList<TeamDetailsFlatDTO>();
+		li = teamoperation.FetchTeamDetails(team);
+		
 		try {
 			 teamoperation.deleteTeam(team);
 		} catch (ClassNotFoundException e) {
@@ -103,6 +120,17 @@ response.sendRedirect("editteam.jsp?msg1=Module Edited");
 			e.printStackTrace();
 		}
 		request.setAttribute("message","Record Deleted");
+		if(session1!= null){
+			reportUpdateDTO.setTeamId(li.get(0).getTeamId());
+			reportUpdateDTO.setTeamName(li.get(0).getTeamName());
+			reportUpdateDTO.setModuleId(li.get(0).getModuleId());
+			reportUpdateDTO.setTeamDescription(li.get(0).getTeamDescription());
+			reportUpdateDTO.setUsername((String)session1.getAttribute("user"));
+			reportUpdateDTO.setAction("deleted");
+			reportUpdateDTO.setTimestamp(timestamp);
+			reportUpdateDTO.setDate(sqlDate);
+			reportController.updateTeamReport(reportUpdateDTO);
+		}
 		request.getRequestDispatcher("viewteam.jsp").forward(request, response);
 		
 		
