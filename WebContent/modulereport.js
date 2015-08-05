61462a8c92b7d8a4194daedbf6519d5d323cdb76
nitin2402 <nitinsaini2402@gmail.com>
2015-08-05 11:52:24 +0530

    sd-REMOVED VALIDATION

--- WebContent/modulereport.js
+++ WebContent/modulereport.js
@@ -1,11 +1,11 @@
 function validateform(){
-	var moduleName = document.forms["modulereport"]["moduleName"].value;
+	/*var moduleName = document.forms["modulereport"]["moduleName"].value;
 	var startDate = document.forms["modulereport"]["startDate"].value;
 	var endDate = document.forms["modulereport"]["endDate"].value;
 	if((moduleName == null || moduleName =="")&& (startDate == null|| startDate == "")&& (endDate == null || endDate == "")){
 		alert("Fields cannot be empty");
 		return false;
-	}
+	}*/
 	if ((Date.parse(startDate) > Date.parse(endDate))) {
         alert("End date should be greater than Start date");
         return false;
