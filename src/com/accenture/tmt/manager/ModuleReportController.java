7f4c5200732bc121a2e7b90bef7a59a7ccf4af44
nitin2402 <nitinsaini2402@gmail.com>
2015-08-05 14:45:54 +0530

    sd-updated cases

--- src/com/accenture/tmt/manager/ModuleReportController.java
+++ src/com/accenture/tmt/manager/ModuleReportController.java
@@ -12,6 +12,12 @@ import com.accenture.tmt.presentation.dto.ModuleReportDTO;
 import com.accenture.tmt.presentation.dto.ModuleReportUpdateDTO;
 
 public class ModuleReportController {
+	public List<ModuleReportFlatDTO>reportModuleWithoutAnything(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsWithoutAnything(modulereportdto);
+		return modulereportflatdto ;
+		}
 	public List<ModuleReportFlatDTO> reportModuleWithoutModuleName(ModuleReportDTO modulereportdto){
 	ModuleReportDAO modulereportdao = new ModuleReportDAO();
 	List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
@@ -19,10 +25,25 @@ public class ModuleReportController {
 	return modulereportflatdto ;
 	}
 	
+	public List<ModuleReportFlatDTO> reportModuleWithoutModuleName_With_Action(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsWithoutModuleName_With_Action(modulereportdto);
+		return modulereportflatdto ;
+		}
+		
+	
 	public List<ModuleReportFlatDTO> reportModuleWithoutStartDate(ModuleReportDTO modulereportdto){
 		ModuleReportDAO modulereportdao = new ModuleReportDAO();
 		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
 		modulereportflatdto = modulereportdao.getDetailsWithoutStartDateDAO(modulereportdto);
+		return modulereportflatdto ;
+		}
+	
+	public List<ModuleReportFlatDTO> reportModuleWithoutStartDate_With_Action(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsWithoutStartDate_With_Action(modulereportdto);
 		return modulereportflatdto ;
 		}
 	
@@ -33,10 +54,26 @@ public class ModuleReportController {
 		return modulereportflatdto ;
 		}
 	
+	public List<ModuleReportFlatDTO> reportModuleWithoutEndDate_With_Action(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsWithoutEndDateDAO_With_Action(modulereportdto);
+		return modulereportflatdto ;
+		}
+	
+	
+	
 	public List<ModuleReportFlatDTO>reportModuleWithOnlyEndDate(ModuleReportDTO modulereportdto){
 		ModuleReportDAO modulereportdao = new ModuleReportDAO();
 		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
 		modulereportflatdto = modulereportdao.getDetailsWithOnlyEndDateDAO(modulereportdto);
+		return modulereportflatdto ;
+		}
+
+	public List<ModuleReportFlatDTO>reportModuleWithOnlyEndDate_With_Action(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsEndDate_With_Action(modulereportdto);
 		return modulereportflatdto ;
 		}
 	
@@ -46,6 +83,12 @@ public class ModuleReportController {
 		modulereportflatdto = modulereportdao.getDetailsWithOnlyModuleNameDAO(modulereportdto);
 		return modulereportflatdto ;
 		}
+	public List<ModuleReportFlatDTO>reportModuleWithOnlyModuleName_With_Action(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsModuleName_With_Action(modulereportdto);
+		return modulereportflatdto ;
+		}
 	
 	public List<ModuleReportFlatDTO>reportModuleWithOnlyStartDate(ModuleReportDTO modulereportdto){
 		ModuleReportDAO modulereportdao = new ModuleReportDAO();
@@ -53,12 +96,37 @@ public class ModuleReportController {
 		modulereportflatdto = modulereportdao.getDetailsWithOnlyStartDateDAO(modulereportdto);
 		return modulereportflatdto ;
 		}
+
+	public List<ModuleReportFlatDTO>reportModuleWithOnlyStartDate_With_Action(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsStartDate_With_Action(modulereportdto);
+		return modulereportflatdto ;
+		}
+
+	public List<ModuleReportFlatDTO>reportModuleWithOnlyAction(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsWithOnlyAction(modulereportdto);
+		return modulereportflatdto ;
+		}
+
 	public List<ModuleReportFlatDTO>reportModule(ModuleReportDTO modulereportdto){
 		ModuleReportDAO modulereportdao = new ModuleReportDAO();
 		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
 		modulereportflatdto = modulereportdao.getDetailsReportDAO(modulereportdto);
 		return modulereportflatdto ;
 		}
+	
+	
+
+	public List<ModuleReportFlatDTO>reportModule_With_Action(ModuleReportDTO modulereportdto){
+		ModuleReportDAO modulereportdao = new ModuleReportDAO();
+		List<ModuleReportFlatDTO> modulereportflatdto = new ArrayList<ModuleReportFlatDTO>();
+		modulereportflatdto = modulereportdao.getDetailsReportDAOWithAction(modulereportdto);
+		return modulereportflatdto ;
+		}
+	
 	
 public  List<String> fetchModuleList(){
 		
