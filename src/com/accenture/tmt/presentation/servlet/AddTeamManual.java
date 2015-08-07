e8e83bce2a5672a62db6c3dc7120bcb17f6f6c50
nitin2402 <nitinsaini2402@gmail.com>
2015-08-07 11:59:18 +0530

    sd-modified for team reports

--- src/com/accenture/tmt/presentation/servlet/AddTeamManual.java
+++ src/com/accenture/tmt/presentation/servlet/AddTeamManual.java
@@ -10,9 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.accenture.tmt.dao.TeamDAO;
 import com.accenture.tmt.dao.dto.TeamDetailsFlatDTO;
 import com.accenture.tmt.manager.TeamController;
 import com.accenture.tmt.manager.TeamReportController;
+import com.accenture.tmt.presentation.dto.ModuleReportUpdateDTO;
 import com.accenture.tmt.presentation.dto.TeamReportUpdateDTO;
 
 /**
@@ -41,14 +43,17 @@ public class AddTeamManual extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	    TeamReportUpdateDTO reportUpdateDTO = new TeamReportUpdateDTO();
-	    TeamReportController reportController = new TeamReportController();
-		HttpSession session1 = request.getSession();
-		
-		String TeamName = request.getParameter("Tname");
-		String Teamid = request.getParameter("Tno");
-		String ModuleId = request.getParameter("Mname");
+	
+
+		String TeamName = request.getParameter("tname");
+		//String Teamid = request.getParameter("project1");
+		String ModuleId = request.getParameter("module");
 		String TeamDesc = request.getParameter("desc");
+		
+		TeamDAO teamDAO= new TeamDAO();
+		int lastCount = teamDAO.countRows();
+		String Teamid = "TE -"+ (lastCount + 1);
+		
 		TeamDetailsFlatDTO detailsDO = new TeamDetailsFlatDTO();
 		detailsDO.setTeamName(TeamName);
 		detailsDO.setTeamId(Teamid);
@@ -56,31 +61,39 @@ public class AddTeamManual extends HttpServlet {
 		detailsDO.setTeamDescription(TeamDesc);
 		
 		java.sql.Date sqlDate=null;
+		
+		HttpSession session1 = request.getSession();
 		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 		Date date = new Date();
-		String timestamp= df.format(date);
-		sqlDate= new java.sql.Date(date.getTime());
-		
+		 String timestamp= df.format(date);
+		 sqlDate= new java.sql.Date(date.getTime());
 		TeamController TeamManager = new TeamController();
-		int c = 0;
-		c = TeamManager.addTeamManual(detailsDO);
-		
-		if(c!=0){
+		TeamReportUpdateDTO reportupdatedto = new TeamReportUpdateDTO();
+		TeamReportController update=new TeamReportController();
+		int c=TeamManager.addTeamManual(detailsDO);
+		TeamDetailsFlatDTO detailsFD = new TeamDetailsFlatDTO();
+		detailsFD.setModuleId(ModuleId);
+		detailsFD.setTeamName(TeamName);
+		detailsFD.setTeamId(Teamid);
+		detailsFD.setTeamDescription(TeamDesc);
+		if(c !=0){
+			
 			request.setAttribute("message","Record Inserted");
 			if(session1!= null){
-				reportUpdateDTO.setTeamId(Teamid);
-				reportUpdateDTO.setTeamName(TeamName);
-				reportUpdateDTO.setModuleId(ModuleId);
-				reportUpdateDTO.setTeamDescription(TeamDesc);
-				reportUpdateDTO.setUsername((String)session1.getAttribute("user"));
-				reportUpdateDTO.setAction("added");
-				reportUpdateDTO.setTimestamp(timestamp);
-				reportUpdateDTO.setDate(sqlDate);
-				reportController.updateTeamReport(reportUpdateDTO);
+		
+			reportupdatedto.setModuleId(ModuleId);
+			reportupdatedto.setTeamId(Teamid);
+			reportupdatedto.setTeamName(TeamName);
+			reportupdatedto.setTeamDescription(TeamDesc);
+			reportupdatedto.setUsername((String)session1.getAttribute("user"));
+			reportupdatedto.setAction("added");
+			reportupdatedto.setTimestamp(timestamp);
+			reportupdatedto.setDate(sqlDate);
+			update.updateTeamReport(reportupdatedto);
 			}
 			
 			request.getRequestDispatcher("admintool.jsp").forward(request, response);}
-		if(c == 0){
+		if(c ==0){
 			request.setAttribute("message","Record insertion failed");
 			request.getRequestDispatcher("admintool.jsp").forward(request, response);}
 		
