fc21a4a36eb79f7069590cd410eb299b2ebe8105
nitin2402 <nitinsaini2402@gmail.com>
2015-08-07 12:14:00 +0530

    sd-updates for team reports

--- src/com/accenture/tmt/presentation/servlet/EditTeamFinal.java
+++ src/com/accenture/tmt/presentation/servlet/EditTeamFinal.java
@@ -44,44 +44,42 @@ public class EditTeamFinal extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		TeamReportUpdateDTO reportUpdateDTO = new TeamReportUpdateDTO();
-		TeamReportController reportController = new TeamReportController();
-		HttpSession session1 = request.getSession();
-		
 		java.sql.Date sqlDate=null;
+		String teamname = request.getParameter("teamname");
+		String module = request.getParameter("module");
+		String teamdesc = request.getParameter("teamdesc");
+		String teamid = request.getParameter("teamid");
+		HttpSession session1 = request.getSession();
 		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 		Date date = new Date();
-		String timestamp= df.format(date);
-		sqlDate= new java.sql.Date(date.getTime());
-		
-		String teamName = request.getParameter("teamname");
-		String module = request.getParameter("module");
-		String teamDesc = request.getParameter("teamdesc");
-		String teamid = request.getParameter("teamid");
-		
+		 String timestamp= df.format(date);
+		 sqlDate= new java.sql.Date(date.getTime());
 		ModuleController id  =new ModuleController();
-		String moduleId = id.fetchModuleid(module); //converting moduleName into moduleId
+		String moduleid = id.fetchModuleid(module); 
 		
-		int a = 0;
 		TeamController edit = new TeamController();
-		a = edit.EditTeam(teamName, moduleId, teamDesc, teamid);
+		TeamReportUpdateDTO reportupdatedto = new TeamReportUpdateDTO();
+		TeamReportController update=new TeamReportController();
+		int a = edit.EditTeam(teamname, moduleid, teamdesc, teamid);
 		if(a!=0)
 		{
-			request.setAttribute("message","Team Updated");
 			if(session1!= null){
-				reportUpdateDTO.setTeamId(teamid);
-				reportUpdateDTO.setTeamName(teamName);
-				reportUpdateDTO.setModuleId(moduleId);
-				reportUpdateDTO.setTeamDescription(teamDesc);
-				reportUpdateDTO.setUsername((String)session1.getAttribute("user"));
-				reportUpdateDTO.setAction("edited");
-				reportUpdateDTO.setTimestamp(timestamp);
-				reportUpdateDTO.setDate(sqlDate);
-				reportController.updateTeamReport(reportUpdateDTO);
+				
+				reportupdatedto.setModuleId(moduleid);
+				reportupdatedto.setTeamId(teamid);
+				reportupdatedto.setTeamName(teamname);
+				reportupdatedto.setTeamDescription(teamdesc);
+				reportupdatedto.setUsername((String)session1.getAttribute("user"));
+				reportupdatedto.setAction("edited");
+				reportupdatedto.setTimestamp(timestamp);
+				reportupdatedto.setDate(sqlDate);
+				update.updateTeamReport(reportupdatedto);
+				}
+			
+			
+			request.setAttribute("message","Team Updated");
+		request.getRequestDispatcher("viewteam.jsp").forward(request, response);
 			}
-			request.getRequestDispatcher("viewteam.jsp").forward(request, response);
-		}
 		if (a==0)
 		{
 			request.setAttribute("message","Team not Updated");
