d4ebcac1edb4dfc8432503704dcd65efbbf6c13c
nitin2402 <nitinsaini2402@gmail.com>
2015-08-07 12:11:25 +0530

    sd-updates for team reports

--- src/com/accenture/tmt/presentation/servlet/EditTeam.java
+++ src/com/accenture/tmt/presentation/servlet/EditTeam.java
@@ -49,22 +49,16 @@ public class EditTeam extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		TeamReportUpdateDTO reportUpdateDTO = new TeamReportUpdateDTO();
-		TeamReportController reportController = new TeamReportController();
-		HttpSession session1 = request.getSession();
-		
-		java.sql.Date sqlDate=null;
-		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-		Date date = new Date();
-		String timestamp= df.format(date);
-		sqlDate= new java.sql.Date(date.getTime());
-		
-		
 		String submit1=request.getParameter("submit1");
 		String submit2=request.getParameter("submit2");
 		String team = request.getParameter("teamedit");
 		TeamController teamoperation = new TeamController();
+		HttpSession session1 = request.getSession();
+		java.sql.Date sqlDate=null;
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		Date date = new Date();
+		 String timestamp= df.format(date);
+		 sqlDate= new java.sql.Date(date.getTime());
 		if(team==null){
 			request.setAttribute("message","Please select the radio button");
 			request.getRequestDispatcher("viewteam.jsp").forward(request, response);	
@@ -73,45 +67,35 @@ public class EditTeam extends HttpServlet {
 		List<TeamDetailsFlatDTO> li = new ArrayList<TeamDetailsFlatDTO>();
 		
 		li = teamoperation.FetchTeamDetails(team);
-		
-	/*if(n_module==module )
-		{
-		n_module=module;
-		}
-	
-	if(n_team ==null || n_team =="")
-	{
-		n_team=team;
-	}*/
-		/*ModuleManager m = new ModuleManager();
-		String mid = m.fetchModuleid(module);*/
-	
-		
-		/*String tid = editteam.fetchTeamid(team,mid);
-		
-		int a =editteam.EditTeam(n_team, mid,tid);
-		if(a>0)
-		{
-response.sendRedirect("editteam.jsp?msg1=Module Edited");
-		}
-		else
-		{
-			
-				response.sendRedirect("editteam.jsp?msg1=Failed to save changes !!");
-						
-		}*/
-		
 		request.setAttribute("team", li);
 		request.getRequestDispatcher("editteam.jsp").forward(request, response);
 	}
 	
 	if(submit2!=null){
-		
-		List<TeamDetailsFlatDTO> li = new ArrayList<TeamDetailsFlatDTO>();
-		li = teamoperation.FetchTeamDetails(team);
-		
+		TeamReportUpdateDTO reportupdatedto = new TeamReportUpdateDTO();
+		TeamReportController update=new TeamReportController();
+	
 		try {
-			 teamoperation.deleteTeam(team);
+			List<TeamDetailsFlatDTO> li = new ArrayList<TeamDetailsFlatDTO>();
+			li = teamoperation.FetchTeamDetails(team);
+			int status= teamoperation.deleteTeam(team);
+			if(status==1){
+				request.setAttribute("message","Team Deleted and Employees Are Set To Free !!!");
+				if(session1!= null){
+					for(int i =0;i<li.size();i++){
+					reportupdatedto.setModuleId(li.get(i).getModuleId());
+					reportupdatedto.setTeamId(team);
+					reportupdatedto.setTeamName(li.get(i).getTeamName());
+					reportupdatedto.setTeamDescription(li.get(i).getTeamDescription());
+					reportupdatedto.setUsername((String)session1.getAttribute("user"));
+					reportupdatedto.setAction("deleted");
+					reportupdatedto.setTimestamp(timestamp);
+					reportupdatedto.setDate(sqlDate);
+					update.updateTeamReport(reportupdatedto);
+					}}
+			}else{
+				request.setAttribute("message","DELETE FAILED!");
+			}
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -119,18 +103,7 @@ response.sendRedirect("editteam.jsp?msg1=Module Edited");
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		request.setAttribute("message","Record Deleted");
-		if(session1!= null){
-			reportUpdateDTO.setTeamId(li.get(0).getTeamId());
-			reportUpdateDTO.setTeamName(li.get(0).getTeamName());
-			reportUpdateDTO.setModuleId(li.get(0).getModuleId());
-			reportUpdateDTO.setTeamDescription(li.get(0).getTeamDescription());
-			reportUpdateDTO.setUsername((String)session1.getAttribute("user"));
-			reportUpdateDTO.setAction("deleted");
-			reportUpdateDTO.setTimestamp(timestamp);
-			reportUpdateDTO.setDate(sqlDate);
-			reportController.updateTeamReport(reportUpdateDTO);
-		}
+	;
 		request.getRequestDispatcher("viewteam.jsp").forward(request, response);
 		
 		
