d09b09c4f40ba18c96614c56d5188b2bcff555f5
nitin2402 <nitinsaini2402@gmail.com>
2015-08-13 11:14:07 +0530

    SD-DTO for emp reports

---
+++ src/com/accenture/tmt/presentation/dto/EmployeeReportUpdateDTO.java
@@ -0,0 +1,147 @@
+package com.accenture.tmt.presentation.dto;
+
+import java.sql.Date;
+
+public class EmployeeReportUpdateDTO {
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getExpertise() {
+		return expertise;
+	}
+	public void setExpertise(String expertise) {
+		this.expertise = expertise;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTeamId() {
+		return teamId;
+	}
+	public void setTeamId(String teamId) {
+		this.teamId = teamId;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getProfCamps() {
+		return profCamps;
+	}
+	public void setProfCamps(String profCamps) {
+		this.profCamps = profCamps;
+	}
+	public String getProfProject() {
+		return profProject;
+	}
+	public void setProfProject(String profProject) {
+		this.profProject = profProject;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+	public String getLastWD() {
+		return lastWD;
+	}
+	public void setLastWD(String lastWD) {
+		this.lastWD = lastWD;
+	}
+	public String getIsBillable() {
+		return isBillable;
+	}
+	public void setIsBillable(String isBillable) {
+		this.isBillable = isBillable;
+	}
+	public String getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	public String getCost() {
+		return cost;
+	}
+	public void setCost(String cost) {
+		this.cost = cost;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	private String empId;
+	private String empName;
+	private String designation;
+
+	private String expertise;
+	private String level;
+	private String clientId;
+	private String email;
+	private String teamId;
+	private String teamName;
+	private String profCamps;
+	private String profProject;
+	private String doj;
+	private String lastWD;
+	private String isBillable;
+	private String isActive;
+	private String cost;
+	private String userName;
+	private String action;
+	private String timeStamp;
+	private Date date;
+}
