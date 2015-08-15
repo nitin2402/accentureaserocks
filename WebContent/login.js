efbabb81bbdf96cf4da999b4b45116342f6d62c7
nitin2402 <nitinsaini2402@gmail.com>
2015-08-15 13:06:04 +0530

    RTM: deleted commented codes

--- WebContent/login.js
+++ WebContent/login.js
@@ -1,21 +1,4 @@
-
-
-
-
-	$(function() {
-		$('.last').click(
-				function(event) {
-					var email = 'chinu.mohanty@accenture.com';
-					var subject = 'Feedback';
-					var emailBody = 'Hi Chinu,';
-					window.location = 'mailto:' + email + '?subject=' + subject
-							+ '&body=' + emailBody;
-				})
-	});
-
-
-
-	function validateform() {
+function validateform() {
 
 		var name = document.forms["login"]["USERNAME"].value;
 		var password = document.forms["login"]["PASSWORD"].value;
@@ -28,14 +11,4 @@
 			alert("Please provide Password");
 			return false;
 		}
-		
-	 function checkUserType(){
-		 /*if (session1.getAttribute("admin") == "ADMIN")
-			 return 1;
-		 else if (session1.getAttribute("admin") == "USER")
-			 return 2;
-		 else 
-			 return 3;*/
-		 alert("Hi");
-	 }	
 	}
