850ba2892104bbbc9123b7bdea39d4404e57ffa6
nitin2402 <nitinsaini2402@gmail.com>
2015-08-17 15:22:53 +0530

    NS: CodeCleanup

--- src/com/accenture/tmt/manager/EmployeeController.java
+++ src/com/accenture/tmt/manager/EmployeeController.java
@@ -13,122 +13,127 @@ import com.accenture.tmt.presentation.dto.PieChartDTO;
 import com.accenture.tmt.presentation.dto.SearchFormDTO;
 
 import static java.lang.System.out;
+
 public class EmployeeController {
-	EmployeeDAO employeeDAO= new EmployeeDAO();
-	
-	public  int deleteEmployee(String empId) throws ClassNotFoundException, SQLException{
-		int status=employeeDAO.deleteEmployee(empId);
+	EmployeeDAO employeeDAO = new EmployeeDAO();
+
+	public int deleteEmployee(String empId) throws ClassNotFoundException,
+			SQLException {
+		int status = employeeDAO.deleteEmployee(empId);
 		return status;
 	}
-	
-	public int addEmployee(EmployeeDetailsDTO emp)
-	{	
-		/*String teamId = null ;
-		WorkplanDAO workplanDao = new WorkplanDAO();
-		teamId = workplanDao.fetchTeamId(emp.getTeamName());*/
-	
-	EmployeeDetailsFlatDTO detailsFDO = new EmployeeDetailsFlatDTO();
-	detailsFDO.setEmpId(emp.getEmpId());
-	detailsFDO.setEmpName(emp.getEmpName());
-	detailsFDO.setLevel(emp.getLevel());
-	detailsFDO.setDesignation(emp.getDesignation());
-	detailsFDO.setExpertise(emp.getExpertise());
-	detailsFDO.setClientId(emp.getClientId());
-	detailsFDO.setEmail(emp.getEmail());
-	detailsFDO.setTeamId(emp.getTeamName());
-	detailsFDO.setProfCamps(emp.getProfCamps());
-	detailsFDO.setProfProject(emp.getProfProject());
-	detailsFDO.setDoj(emp.getDoj());
-	detailsFDO.setLastWD(emp.getLastWD());
-	detailsFDO.setIsBillable(emp.getIsBillable());
-	detailsFDO.setIsActive(emp.getIsActive());
-	detailsFDO.setCost(emp.getCost());
-	EmployeeDAO dao=new EmployeeDAO();
-	int staus=dao.addEmployee(detailsFDO);
 
-	if(staus!=0){
-		out.println("Record Inserted");
+	public int addEmployee(EmployeeDetailsDTO emp) {
+
+		EmployeeDetailsFlatDTO detailsFDO = new EmployeeDetailsFlatDTO();
+		detailsFDO.setEmpId(emp.getEmpId());
+		detailsFDO.setEmpName(emp.getEmpName());
+		detailsFDO.setLevel(emp.getLevel());
+		detailsFDO.setDesignation(emp.getDesignation());
+		detailsFDO.setExpertise(emp.getExpertise());
+		detailsFDO.setClientId(emp.getClientId());
+		detailsFDO.setEmail(emp.getEmail());
+		detailsFDO.setTeamId(emp.getTeamName());
+		detailsFDO.setProfCamps(emp.getProfCamps());
+		detailsFDO.setProfProject(emp.getProfProject());
+		detailsFDO.setDoj(emp.getDoj());
+		detailsFDO.setLastWD(emp.getLastWD());
+		detailsFDO.setIsBillable(emp.getIsBillable());
+		detailsFDO.setIsActive(emp.getIsActive());
+		detailsFDO.setCost(emp.getCost());
+		EmployeeDAO dao = new EmployeeDAO();
+		int staus = dao.addEmployee(detailsFDO);
+
+		if (staus != 0) {
+			out.println("Record Inserted");
+		}
+		if (staus == 0) {
+			out.println("Record insertion failed");
+
+		}
+		return staus;
+
 	}
-	if(staus ==0){
-		out.println("Record insertion failed");
-	
-}
-	return staus;
-	
-	}
-	
-	public List<EmployeeDetailsFlatDTO> viewEmployee(){
-		List<EmployeeDetailsFlatDTO> empList= new ArrayList<EmployeeDetailsFlatDTO>();
+
+	public List<EmployeeDetailsFlatDTO> viewEmployee() {
+		List<EmployeeDetailsFlatDTO> empList = new ArrayList<EmployeeDetailsFlatDTO>();
 		empList = employeeDAO.viewEmployee();
 		return empList;
-		
+
 	}
-public List<String> fetchTeamList(){
-		
+
+	public List<String> fetchTeamList() {
+
 		WorkplanDAO workplanDao = new WorkplanDAO();
 		return workplanDao.getTeamList();
-		 
+
 	}
-	
-	/** zfdfsdfsd
+
+	/**
+	 * zfdfsdfsd
+	 * 
 	 * @param employeeDetailsDTO
 	 * @return List<SearchFormDTO> - employeeList
 	 * @author n.a.saini
 	 */
 	public List<SearchFormDTO> fetchEmployeeDetails(
 			EmployeeDetailsFlatDTO employeeDetailsDTO) {
-			List<SearchFormDTO> employeeList = employeeDAO.fetchEmployeeDetails(employeeDetailsDTO);
-			return employeeList;
-			}
-	
-	public int fetchEmployeeCount(String team,String desig){
-		int empCount=employeeDAO.fetchEmployeeCount(team, desig);
-		return empCount;
-			}
-	
-	public List<EmployeeDetailsFlatDTO> FetchEmployeeList(String empId){
-		List<EmployeeDetailsFlatDTO> employeeList=employeeDAO.fetchEmployeeList(empId);
+		List<SearchFormDTO> employeeList = employeeDAO
+				.fetchEmployeeDetails(employeeDetailsDTO);
 		return employeeList;
-		
+	}
+
+	public int fetchEmployeeCount(String team, String desig) {
+		int empCount = employeeDAO.fetchEmployeeCount(team, desig);
+		return empCount;
+	}
+
+	public List<EmployeeDetailsFlatDTO> FetchEmployeeList(String empId) {
+		List<EmployeeDetailsFlatDTO> employeeList = employeeDAO
+				.fetchEmployeeList(empId);
+		return employeeList;
+
 	}
 
 	public int EditEmployee(EmployeeDetailsFlatDTO employeeDetailsDTO) {
-			int status=employeeDAO.editEmployee(employeeDetailsDTO);
-			return status;
-		}
-	
-	public int addEmployeeFinal(String s_empId,String s_team){
-		int status=employeeDAO.addEmployeeFinal(s_empId, s_team);
+		int status = employeeDAO.editEmployee(employeeDetailsDTO);
 		return status;
-		
 	}
-	
+
+	public int addEmployeeFinal(String s_empId, String s_team) {
+		int status = employeeDAO.addEmployeeFinal(s_empId, s_team);
+		return status;
+
+	}
+
 	public List<SearchFormDTO> searchEmployee(String name) {
 
-		
 		List<SearchFormDTO> searchlist = new ArrayList<SearchFormDTO>();
-		
-		searchlist=employeeDAO.searchEmployee(name);
+
+		searchlist = employeeDAO.searchEmployee(name);
 		return searchlist;
-		
-		}
-	
+
+	}
+
 	public List<EmployeeDetailsFlatDTO> getToAssign(String employee) {
 
-		List<EmployeeDetailsFlatDTO> employeeList=employeeDAO.getToAssign(employee);
-				return employeeList;
-       }
-	public List<pieCount> fetchPieDetails(PieChartDTO pieDTO) {
-		List<pieCount> piechartlist= new ArrayList<pieCount>();
-		piechartlist=EmployeeDAO.fetchPieDetails(pieDTO);
-		return piechartlist;
-		// TODO Auto-generated method stub
-		
-	}
-	public List<EmployeeDetailsFlatDTO> FetchEmployeeListForTeam(String teamId){
-		List<EmployeeDetailsFlatDTO> employeeList=employeeDAO.fetchEmployeeListForTeam(teamId);
+		List<EmployeeDetailsFlatDTO> employeeList = employeeDAO
+				.getToAssign(employee);
 		return employeeList;
-		
 	}
-	
+
+	public List<pieCount> fetchPieDetails(PieChartDTO pieDTO) {
+		List<pieCount> piechartlist = new ArrayList<pieCount>();
+		piechartlist = EmployeeDAO.fetchPieDetails(pieDTO);
+		return piechartlist;
+
+	}
+
+	public List<EmployeeDetailsFlatDTO> FetchEmployeeListForTeam(String teamId) {
+		List<EmployeeDetailsFlatDTO> employeeList = employeeDAO
+				.fetchEmployeeListForTeam(teamId);
+		return employeeList;
+
+	}
+
 }
