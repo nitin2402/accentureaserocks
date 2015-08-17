71ebbeb4aef003fc6d8bac4fd4c237d524584747
nitin2402 <nitinsaini2402@gmail.com>
2015-08-17 15:24:12 +0530

    NS: Code CleanUp

--- src/com/accenture/tmt/manager/TeamController.java
+++ src/com/accenture/tmt/manager/TeamController.java
@@ -12,26 +12,25 @@ import com.accenture.tmt.dao.dto.TeamDetailsFlatDTO;
 import com.accenture.tmt.presentation.dto.TeamFormDTO;
 
 public class TeamController {
-	TeamDAO teamDAO= new TeamDAO();
-	
-	public  int deleteTeam(String teamID) throws ClassNotFoundException, SQLException{
-		int status=teamDAO.deleteTeam(teamID);
+	TeamDAO teamDAO = new TeamDAO();
+
+	public int deleteTeam(String teamID) throws ClassNotFoundException,
+			SQLException {
+		int status = teamDAO.deleteTeam(teamID);
 		return status;
 	}
-	public int addTeamManual(TeamDetailsFlatDTO detailsDO)
-	{
-		//String ModuleId = null;
+
+	public int addTeamManual(TeamDetailsFlatDTO detailsDO) {
+
 		WorkplanDAO workplanDao = new WorkplanDAO();
-		//ModuleId = workplanDao.fetchModuleId(detailsDO.getModuleName());
-		/*int lastCount = teamDAO.countRows();
-		String newReqId = "TE -"+ (lastCount + 1);*/
+
 		TeamDetailsFlatDTO detailsFD = new TeamDetailsFlatDTO();
 		detailsFD.setTeamName(detailsDO.getTeamName());
-		/*detailsFD.setTeamId(newReqId);*/
+
 		detailsFD.setTeamId(detailsDO.getTeamId());
 		detailsFD.setModuleId(detailsDO.getModuleId());
 		detailsFD.setTeamDescription(detailsDO.getTeamDescription());
-		
+
 		int b = teamDAO.addTeamManually(detailsFD);
 
 		if (b != 0) {
@@ -44,51 +43,54 @@ public class TeamController {
 
 	}
 
-	
-	public String fetchTeamid(String team , String mid){
-		String status=teamDAO.fetchTeam(team, mid);
-		return status;
-		}
-	
-	public  List<String> fetchModule() {
-		List<String> ModuleList=teamDAO.fetchModule();
-		return ModuleList;
-		}
-	
-	public  List<String> fetchTeams(String moduleId) {
-		List<String> TeamList=teamDAO.fetchTeams(moduleId);
-		return TeamList;
-		}
-	
-	public int EditTeam(String teamname , String module ,String teamdescription,String teamId) {
-		int status=teamDAO.editTeam(teamname, module, teamdescription, teamId);
+	public String fetchTeamid(String team, String mid) {
+		String status = teamDAO.fetchTeam(team, mid);
 		return status;
 	}
-	
-	public List<TeamDetailsFlatDTO> FetchTeamDetails(String teamId)
-	{
-		List<TeamDetailsFlatDTO> TeamDetailsList=teamDAO.fetchTeamDetails(teamId);
+
+	public List<String> fetchModule() {
+		List<String> ModuleList = teamDAO.fetchModule();
+		return ModuleList;
+	}
+
+	public List<String> fetchTeams(String moduleId) {
+		List<String> TeamList = teamDAO.fetchTeams(moduleId);
+		return TeamList;
+	}
+
+	public int EditTeam(String teamname, String module, String teamdescription,
+			String teamId) {
+		int status = teamDAO
+				.editTeam(teamname, module, teamdescription, teamId);
+		return status;
+	}
+
+	public List<TeamDetailsFlatDTO> FetchTeamDetails(String teamId) {
+		List<TeamDetailsFlatDTO> TeamDetailsList = teamDAO
+				.fetchTeamDetails(teamId);
 		return TeamDetailsList;
 	}
-	
-	public  List<String> fetchTeam(String module) {
-		List<String> teamList=teamDAO.fetchTeam(module);
+
+	public List<String> fetchTeam(String module) {
+		List<String> teamList = teamDAO.fetchTeam(module);
 		return teamList;
-    }
-	
-	public List<TeamDetailsFlatDTO> viewTeam(){
+	}
+
+	public List<TeamDetailsFlatDTO> viewTeam() {
 		List<TeamDetailsFlatDTO> list = new ArrayList<TeamDetailsFlatDTO>();
-		list=teamDAO.viewTeam();
+		list = teamDAO.viewTeam();
 		return list;
 	}
-	public  List<TeamFormDTO> fetchTeamDetails() {
-		List<TeamFormDTO> teamList=teamDAO.fetchTeamDetails();
+
+	public List<TeamFormDTO> fetchTeamDetails() {
+		List<TeamFormDTO> teamList = teamDAO.fetchTeamDetails();
 		return teamList;
 	}
-	public  List<TeamFormDTO> teamFetch() {
-		List<TeamFormDTO> teamList=teamDAO.teamFetch();
-		//teamList=teamDAO.teamFetch();
+
+	public List<TeamFormDTO> teamFetch() {
+		List<TeamFormDTO> teamList = teamDAO.teamFetch();
+
 		return teamList;
 	}
-	
+
 }
