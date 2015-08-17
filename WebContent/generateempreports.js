52c6653627bb0e49b600d67daa606cc73c2e3005
nitin2402 <nitinsaini2402@gmail.com>
2015-08-17 17:44:16 +0530

    sd-accordian js

--- WebContent/generateempreports.js
+++ WebContent/generateempreports.js
@@ -1,12 +1,19 @@
 	$(document).ready(function() {
 		$("#div2,#div1").hide();
+		  $( "#accordion" ).accordion();
 	});
 
+	
+	 
+	 $(function(){$("#mdl").chained("#prj");
+		$("#tem").chained("#mdl")});
+	 
 	function changeTextBox() {
 		var val = $('#select').val();
 
 		if (val === 'default') {
 			$("#div2,#div1").hide();
+			
 		}
 
 		else if (val === 'employee') {
