076da5d6ac2a6dc5b2ceacb7057f410121abc9d6
nitin2402 <nitinsaini2402@gmail.com>
2015-08-17 14:38:14 +0530

    pk: for pdf generate

---
+++ src/com/accenture/tmt/dao/PDFDao.java
@@ -0,0 +1,50 @@
+package com.accenture.tmt.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.accenture.tmt.common.CONSTANTS;
+import com.accenture.tmt.common.DBConnection;
+import com.accenture.tmt.dao.dto.EmployeeDetailsFlatDTO;
+public class PDFDao {
+	
+	public EmployeeDetailsFlatDTO fetchEmployeedetails(String userName) {
+		EmployeeDetailsFlatDTO emp = new EmployeeDetailsFlatDTO();
+		try {
+			Connection con = DBConnection.getConnection();
+			PreparedStatement st = con
+					.prepareStatement("SELECT Employee.EmployeeId,Employee.EmployeeClientId, Employee.EmployeeName, Employee.EmployeeLevel, Employee.EmployeeDesignation, Employee.EmployeeEmail, Employee.LastWorkingDay "
+							+"FROM Employee, Login "
+							+"where Employee.EmployeeId=Login.EmployeeId "
+							+"and Login.UserName=?");
+			st.setString(1, userName);
+			ResultSet rs = st.executeQuery();
+		
+			while (rs.next()) {
+				
+				emp.setEmpName(rs.getString(CONSTANTS.EMPLOYEE_NAME));
+				emp.setDesignation(rs.getString(CONSTANTS.EMPLOYEE_DESIGNATION));
+				emp.setLevel(rs.getString(CONSTANTS.EMPLOYEE_LEVEL));				
+				emp.setEmail(rs.getString(CONSTANTS.EMPLOYEE_EMAIL));
+				emp.setLastWD(rs.getString(CONSTANTS.LAST_WORKING_DAY));
+				emp.setEmpId(rs.getString(CONSTANTS.EMPLOYEE_ID));
+				emp.setClientId(rs.getString(CONSTANTS.EMPLOYEE_CLIENTID));
+				
+			}
+		
+			con.close();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return emp;
+
+	}
+}
