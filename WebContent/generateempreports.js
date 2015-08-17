8b19287d6fa93a7fa746427c943ca2646efcab4a
nitin2402 <nitinsaini2402@gmail.com>
2015-08-17 18:42:33 +0530

    sd-accordian js

--- WebContent/generateempreports.js
+++ WebContent/generateempreports.js
@@ -26,3 +26,16 @@
 			$("#div1").hide();
 		}
 	}
+	function validateform(){
+		var status = document.forms["request"]["status"].value;
+		var startdate = document.forms["request"]["startdate"].value;
+		var enddate = document.forms["request"]["enddate"].value;
+		if((status == null || status =="")&& (startdate == null|| startdate == "")&& (enddate == null || enddate == "")){
+			alert("Fields cannot be empty");
+			return false;
+		}
+		if ((Date.parse(startdate) > Date.parse(enddate))) {
+	        alert("End date should be greater than Start date");
+	        return false;
+	    }
+	}
