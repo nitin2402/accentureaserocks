374dbb9c69686891ba3495bd9e2dd26ec32319e7
nitin2402 <nitinsaini2402@gmail.com>
2015-08-17 18:49:06 +0530

    sd-accordian js

--- WebContent/generateempreports.js
+++ WebContent/generateempreports.js
@@ -26,16 +26,44 @@
 			$("#div1").hide();
 		}
 	}
-	function validateform(){
-		var status = document.forms["request"]["status"].value;
-		var startdate = document.forms["request"]["startdate"].value;
-		var enddate = document.forms["request"]["enddate"].value;
-		if((status == null || status =="")&& (startdate == null|| startdate == "")&& (enddate == null || enddate == "")){
-			alert("Fields cannot be empty");
-			return false;
+	$(document).ready(function() {
+		$("#div2,#div1").hide();
+		  $( "#accordion" ).accordion();
+	});
+
+	
+	 
+	 $(function(){$("#mdl").chained("#prj");
+		$("#tem").chained("#mdl")});
+	 
+	function changeTextBox() {
+		var val = $('#select').val();
+
+		if (val === 'default') {
+			$("#div2,#div1").hide();
+			
 		}
-		if ((Date.parse(startdate) > Date.parse(enddate))) {
-	        alert("End date should be greater than Start date");
-	        return false;
-	    }
+
+		else if (val === 'employee') {
+			$("#div2").hide();
+			$("#div1").show();
+		}
+
+		else if (val === 'designation') {
+			$("#div2").show();
+			$("#div1").hide();
+		}
+	}
+	function validateform()
+	{
+	
+	
+	var from = $("#calendar").val(); 
+	var to = $("#calendar1").val();
+
+
+	if(Date.parse(from) > Date.parse(to)){
+	   alert("Last Date Of Working must be after Date Of Joining");
+	   return false;
+	}
 	}
