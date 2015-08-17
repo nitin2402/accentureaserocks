81b8515039817d0dc244eebe648b493b5dc176c9
nitin2402 <nitinsaini2402@gmail.com>
2015-08-17 15:53:35 +0530

    Code cleanup

--- src/com/accenture/tmt/manager/SignupController.java
+++ src/com/accenture/tmt/manager/SignupController.java
@@ -1,15 +1,17 @@
 package com.accenture.tmt.manager;
 
 import com.accenture.tmt.dao.SignupDAO;
-import com.accenture.tmt.dao.dto.SignupFlatDTO;
-import com.accenture.tmt.presentation.dto.RegistrationDTO;
+import com.accenture.tmt.dao.dto.LoginFlatDTO;
+
+import com.accenture.tmt.presentation.dto.LoginDTO;
+
 
 public class SignupController {
 	public int UserRegistration(LoginDTO registerCred){
 		 LoginFlatDTO signupFlatDto = new LoginFlatDTO();
-   	     signupFlatDto.setPassword(registerCred.getPassword());
-   	     signupFlatDto.setUsername(registerCred.getUsername());
-   	     signupFlatDto.setEmployeeid(registerCred.getEmployeeid());
+   	     signupFlatDto.setPassWord(registerCred.getPassword());
+   	     signupFlatDto.setUserName(registerCred.getUsername());
+   	     signupFlatDto.setEmployeeId(registerCred.getEmployeeId());
    	     
    	     SignupDAO signupDAO = new SignupDAO();
    	    int returnregisteruser =  signupDAO.RegisterInDatabase(signupFlatDto);
