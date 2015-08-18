859b6e56a53f924ae784bb3d83c4db1dfa603df3
nitin2402 <nitinsaini2402@gmail.com>
2015-08-18 14:53:46 +0530

    sd-uodated datepicker ids and code clean up

--- WebContent/generateempreports.js
+++ WebContent/generateempreports.js
@@ -1,12 +1,15 @@
-	$(document).ready(function() {
+//accordion	
+$(document).ready(function() {
 		$("#div2,#div1").hide();
 		  $( "#accordion" ).accordion();
 	});
 
-	
+$(document).ready(function() {
+	$("#div2,#div1").hide();
+	  $( "#accordion" ).accordion();
+});
 	 
-	 $(function(){$("#mdl").chained("#prj");
-		$("#tem").chained("#mdl")});
+	//drop down
 	 
 	function changeTextBox() {
 		var val = $('#select').val();
@@ -26,44 +29,24 @@
 			$("#div1").hide();
 		}
 	}
-	$(document).ready(function() {
-		$("#div2,#div1").hide();
-		  $( "#accordion" ).accordion();
-	});
+	
 
 	
-	 
+	 //chained drop down project-module-team
 	 $(function(){$("#mdl").chained("#prj");
 		$("#tem").chained("#mdl")});
 	 
-	function changeTextBox() {
-		var val = $('#select').val();
-
-		if (val === 'default') {
-			$("#div2,#div1").hide();
-			
-		}
-
-		else if (val === 'employee') {
-			$("#div2").hide();
-			$("#div1").show();
-		}
-
-		else if (val === 'designation') {
-			$("#div2").show();
-			$("#div1").hide();
-		}
-	}
-	/*function validateform()
+	//calendar
+	function validateform()
 	{
 	
 	
-	var from = $("#calendar").val(); 
-	var to = $("#calendar1").val();
+	var from = $("#calendar,#calendar2,#calendar4,#calendar6,#calendar8").val(); 
+	var to = $("#calendar1,#calendar3,#calendar5,#calendar7,#calendar9").val();
 
 
 	if(Date.parse(from) > Date.parse(to)){
 	   alert("Last Date Of Working must be after Date Of Joining");
 	   return false;
 	}
-	}*/
+	}
