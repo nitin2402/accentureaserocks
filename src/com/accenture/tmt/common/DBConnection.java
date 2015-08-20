e997868e24a7fe1682fd69ca1462a99d3031dcf8
nitin2402 <nitinsaini2402@gmail.com>
2015-08-20 12:38:29 +0530

    NS: Updated Db connection

--- src/com/accenture/tmt/common/DBConnection.java
+++ src/com/accenture/tmt/common/DBConnection.java
@@ -1,21 +1,41 @@
 package com.accenture.tmt.common;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
+	 static Properties prop = new Properties();
+	static InputStream input = null;
 	private static Connection con;
 	private DBConnection(){
 		
 	}
-	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+	public  static Connection getConnection() throws ClassNotFoundException, SQLException{
+		    try {
+		    	
+		    	
+		    	Properties prop=new Properties();
+		    	prop.load(DBConnection.class.getClassLoader()
+	                    .getResourceAsStream("config.properties"));
+		    	String db=prop.getProperty("DB");
+				Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+				con = DriverManager.getConnection(db);
+				 
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    return con;
 		
 			
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-			 con = DriverManager
-					.getConnection("jdbc:ucanaccess://C:/Users/chinu.mohanty/Desktop/EmployeeDB.accdb");
-			 return con;
 	}
 	
 }
