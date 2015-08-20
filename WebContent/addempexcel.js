f787eea6597e6b7e1e7ff670f489aefffa42f56a
nitin2402 <nitinsaini2402@gmail.com>
2015-08-20 16:12:00 +0530

    SD-hardcoded sheet no.

--- WebContent/addempexcel.js
+++ WebContent/addempexcel.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     });
 
 
-	function SelectSheetNo(e) {
+	/*function SelectSheetNo(e) {
 		var sheet = prompt("please enter sheet number", "1");
 
 		$("#sheetno").val(sheet);
@@ -20,47 +20,51 @@ $(document).ready(function(){
 			 alert("PLEASE ENSURE THAT THE PROJECT,MODULE,TEAM names are correct.YOU MAY REFER TO SHEET 2,3,4 OF THE ABOVE LINK."); 
 			return true;
 		}
-	}
+	}*/
 	
 	var validFileExtensions = [".xlsx"];    
 	function Validate(vaildateObj) {
 	    var getFileName = vaildateObj.getElementsByTagName("input");
 	  
-	    for (var i = 0; i < getFileName.length; i++) {
-	        var fileInput = getFileName[i];
-	        if (fileInput.type == "file")
-	        	  var fileName = fileInput.value;
-	        {
-	        	  if (fileName == "" ||fileName==null || fileName==0)  
-	                  
-	              {
-	              	alert("please select a excel file to upload");
-	              		return false;
-	              } 
-	        	  else{
-	          
-	            if (fileName.length > 0) {
-	                var isValid = false;
-	                for (var j = 0; j < validFileExtensions.length; j++) {
-	                    var extensions = validFileExtensions[j];
-	                    if (fileName.substr(fileName.length - extensions.length, extensions.length).toLowerCase() == extensions.toLowerCase()) {
-	                        isValid = true;
-	                       
-	                        return SelectSheetNo();
-	                    }
-	                }  
-	                if (!isValid) {
-	                    alert("Sorry " + fileName + " is invalid, allowed extensions are: " + validFileExtensions.join(", "));
-	                    return false;
-	                }
-	                
-	            }
-	            
-	            
-	            }
-	          
-	        }
-	    }
-	  
-	    
+
+		    for (var i = 0; i < getFileName.length; i++) {
+		var fileInput = getFileName[i];
+		if (fileInput.type == "file")
+			var fileName = fileInput.value;
+		{
+			if (fileName == "" || fileName == null || fileName == 0)
+
+			{
+				alert("please select a excel file to upload");
+				return false;
+			} else {
+
+				if (fileName.length > 0) {
+					var isValid = false;
+					for (var j = 0; j < validFileExtensions.length; j++) {
+						var extensions = validFileExtensions[j];
+						if (fileName.substr(
+								fileName.length - extensions.length,
+								extensions.length).toLowerCase() == extensions
+								.toLowerCase()) {
+							isValid = true;
+							alert("PLEASE ENSURE THAT THE PROJECT,MODULE,TEAM names are correct.YOU MAY REFER TO SHEET 2,3,4 OF THE ABOVE LINK.");
+							// return SelectSheetNo();
+							return true;
+						}
+					}
+					if (!isValid) {
+						alert("Sorry " + fileName
+								+ " is invalid, allowed extensions are: "
+								+ validFileExtensions.join(", "));
+						return false;
+					}
+
+				}
+
+			}
+
+		}
 	}
+
+}
