019223cdd51f537610a9d343a16e0030e0b84922
nitin2402 <nitinsaini2402@gmail.com>
2015-08-20 18:15:15 +0530

    sd-added validations for addteam manually jsp

--- WebContent/addteamvia.js
+++ WebContent/addteamvia.js
@@ -6,3 +6,42 @@ function validateformteam() {
 	 return false;
 	}
 }
+
+$(function(){$("#mdl").chained("#prj");
+$("#tem").chained("#mdl")});
+
+
+$(document).ready(function(){
+    $("#teamCss").css({
+           	"font-weight": "bold",
+        	"text-decoration": "underline"
+    });
+});
+
+function validateform()
+{
+var name=document.forms["add"]["tname"].value;
+var id=document.forms["add"]["project"].value;
+var level=document.forms["add"]["module"].value;
+var designation=document.forms["add"]["desc"].value;
+if (name == null || name=="")
+  {
+  alert("Please provide Team Name");
+  return false;
+  }
+if (id == null || id=="")
+  {
+  alert("Please provide Team ID");
+  return false;
+  }
+if (level == null || level=="")
+  {
+  alert("Please provide Module Name");s
+  return false;
+  }
+if (designation == null || designation=="")
+  {
+  alert("Please provide Team Description");
+  return false;
+  }
+}
