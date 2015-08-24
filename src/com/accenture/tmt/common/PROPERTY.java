7732cbd6c6d40aa6aa4f61e16c1fc688bd9b0741
nitin2402 <nitinsaini2402@gmail.com>
2015-08-24 18:19:53 +0530

    added project-sd

--- src/com/accenture/tmt/common/PROPERTY.java
+++ src/com/accenture/tmt/common/PROPERTY.java
@@ -2,5 +2,7 @@ package com.accenture.tmt.common;
 
 public interface PROPERTY {
 	public String FETCHDETAILS="SELECT Team.TeamId FROM ProjectDetail INNER JOIN ModuleDetail ON ProjectDetail.ProjectId=ModuleDetail.ProjectId INNER JOIN Team ON Team.ModuleId=ModuleDetail.ModuleId where ProjectDetail.ProjectName=? AND ModuleDetail.ModuleName=? AND Team.TeamName=?";
+	 public String MODULEDETAILS="SELECT  ModuleDetail.ModuleId FROM FROM ProjectDetail INNER JOIN ModuleDetail ON ProjectDetail.ProjectId=ModuleDetail.ProjectId where ProjectDetail.ProjectName=? AND ModuleDetail.ModuleName=?"
+;
 
 }
