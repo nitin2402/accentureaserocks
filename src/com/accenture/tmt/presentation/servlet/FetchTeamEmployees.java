49235756bc40d394a82a4f145e04d028f64959ee
nitin2402 <nitinsaini2402@gmail.com>
2015-08-24 14:52:04 +0530

    Re-Arranging Project Tree

--- src/com/accenture/tmt/presentation/servlet/FetchTeamEmployees.java
+++ src/com/accenture/tmt/presentation/servlet/FetchTeamEmployees.java
@@ -1,4 +1,5 @@
 package com.accenture.tmt.presentation.servlet;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -10,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.accenture.tmt.manager.EmployeeController;
 import com.accenture.tmt.manager.ModuleController;
 import com.accenture.tmt.manager.TeamController;
-
-
 
 /**
  * Servlet implementation class FetchTeamEmployees
@@ -45,44 +44,23 @@ public class FetchTeamEmployees extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		String team = request.getParameter("teamName");
-		String module = request.getParameter("moduleName");
-		
-		
-		
-		ModuleController m = new ModuleController();
-		String mid = m.fetchModuleid(module);
-		
-		
-		
-		TeamController t =new TeamController();
-		String tid = t.fetchTeamid(team, mid);
-	
+		String team = request.getParameter("teamId");
 
-		
 		int noOfASE = 0;
 		int noOfSE = 0;
 		int noOfSSE = 0;
-		int noOfTL=0;
+		int noOfTL = 0;
 
 		EmployeeController countEmp = new EmployeeController();
 
-		noOfASE = countEmp.fetchEmployeeCount(tid, "ASE");
-		noOfSE = countEmp.fetchEmployeeCount(tid, "SE");
-		noOfSSE = countEmp.fetchEmployeeCount(tid, "SSE");
-	    noOfTL=countEmp.fetchEmployeeCount(tid, "TL");
-		
+		noOfASE = countEmp.fetchEmployeeCount(team, "ASE");
+		noOfSE = countEmp.fetchEmployeeCount(team, "SE");
+		noOfSSE = countEmp.fetchEmployeeCount(team, "SSE");
+		noOfTL = countEmp.fetchEmployeeCount(team, "TL");
 
 		PrintWriter print = response.getWriter();
-		print.print("<table id=\"hi\" class=\"table2\" border=\"1\"><tr><td>ASE:</td><td name=\"ase\" id=\"ase\">" + noOfASE
-				+ "</td></tr><tr><td>SE:</td><td name=\"se\" id=\"se\">" + noOfSE
-				+ "</td></tr><tr><td>SSE:</td><td name=\"sse\" id=\"sse\">" + noOfSSE + "</td></tr>"
-						+ "<tr><td>TL:</td><td name=\"tl\" id=\"tl\">" + noOfTL + "</td></tr>"
-								+ "</table><form action=\"DisplayEmpList\">"
-						+ "	<input type=\"hidden\"  name=\"teamnam\" value=\""+tid+"\">"
-						+ "<input type=\"submit\" value=\"Show Employees\"></form><br<br>");
-
-		
+		print.print("{\"ASE\":" + noOfASE + ",\"SE\":" + noOfSE + ",\"SSE\":"
+				+ noOfSSE + ",\"TL\":" + noOfTL + "}");
 
 	}
 
