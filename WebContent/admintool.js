09f4a1cd3f9cc301f7e5a5ae0e43222f7f2e2375
nitin2402 <nitinsaini2402@gmail.com>
2015-08-31 16:40:39 +0530

    sd-sinle admintool page

--- WebContent/admintool.js
+++ WebContent/admintool.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
-
+	 $( "#accordion" ).accordion();
+		$("#employee,#authorize,#modl,#team,#emp").hide();
+	  collapsible: true;
         $("#empCss").css({
            
             	"font-weight": "bold",
@@ -8,25 +10,148 @@ $(document).ready(function(){
         });
     });
 
-	$(document).ready(function() {
-		$(".header_09").hide();
-	});
+function changeTextBox() {
+	var val = $('#select').val();
+	var val1 = $('#select1').val();
+	/*var val = $('#select1').val();*/
+	if (val === 'default') {
+		$("#employee,#authorize").hide();
+		
+	}
+
+	else if (val === 'employee') {
+		$("#authorize").hide();
+		$("#employee").show();
+	}
+
+	else if (val === 'authorize') {
+		$("#authorize").show();
+		$("#employee").hide();
+	}
+	
+	if (val1 === 'default') {
+		$("#modl,#team,#emp").hide();
+		
+	}else if (val1 === 'modl1') {
+		$("#modl").show();
+		$("#emp,#team").hide();
+	}else if (val1 === 'team1') {
+		$("#team").show();
+		$("#emp,#modl").hide();
+	}else if (val1 === 'emp1') {
+		$("#emp").show();
+		$("#modl,#team").hide();
+	}
+
+}
+
+
 
 
 
  
 
-	function validateform2(e) {
+function validateform2() {
 
-	if ( ( !document.getElementById('excel').checked ) && ( !document.getElementById('manually').checked ) ) 
-
+	if ( ( !document.getElementById('excel').checked  ) && ( !document.getElementById('manually').checked  ) ) 
 	{
+	 alert ( "Please choose one option" );
+	 return false;
+	}
+	if ( ( !document.getElementById('excel1').checked  ) && ( !document.getElementById('manually1').checked  ) ) 
+	{
+	 alert ( "Please choose one option" );
+	 return false;
+	}
+	if ( ( !document.getElementById('excel2').checked  ) && ( !document.getElementById('manually2').checked  ) ) 
+	{
+	 alert ( "Please choose one option" );
+	 return false;
+	}
+}
 
-	alert ( "Please choose one option" );
 
-	return false;
+	function validateAdmin(){
+		
 
+		 if ($("input[name=select]").is(":checked")) {
+			 
+			var id = $("input[name=select]:checked").val();
+		   var desig = $('#'+id).val();
+	 
+		if(desig == 'ASE' || desig == 'SE' || desig == 'SSE' || desig == 'TL'){
+		    var answer = confirm('Designation Mismatch !!! Do you want to approve?');
+			if (answer)
+			{
+			  return true;
+			}
+			else
+			{
+				  return false;
+			}
+			 
+		}
+		else if(desig == 'AM' || desig == 'M' || desig == 'SM' || desig == 'Accenture Leadership'){
+			return true;
+		}
+		 }else{
+			   alert("nothing is checked");
+			   return false;
+			 }
+	}
+		
+		
+
+
+	function validateUser(){
+		if ($("input[name=select]").is(":checked")) {
+		var id = $("input[name=select]:checked").val();
+		var desig = $('#'+id).val();
+		
+		if(desig == 'ASE' || desig == 'SE' || desig == 'SSE' ){
+		    var answer = confirm('Designation Mismatch !!! Do you want to approve?');
+			if (answer)
+			{
+			  return true;
+			}
+			else
+			{
+				  return false;
+			}
+			 
+		}
+		else if( desig == 'TL'|| desig == 'AM' || desig == 'M' || desig == 'SM' || desig == 'Accenture Leadership'){
+			return true;
+		}
+		}else{
+			   alert("nothing is checked");
+			   return false;
+			 }
+		
 	}
 
+	function validateViewer(){
+		if ($("input[name=select]").is(":checked")) {
+		var id = $("input[name=select]:checked").val();
+		var desig = $('#'+id).val();
+		 if( desig == 'TL'|| desig == 'AM' || desig == 'M' || desig == 'SM' || desig == 'Accenture Leadership'){
+			var answer = confirm('Designation Mismatch !!! Do you want to approve?');
+			if (answer)
+			{
+			  return true;
+			}
+			else
+			{
+		      return false;
+			}
+		 }
+			else if(desig == 'ASE' || desig == 'SE' || desig == 'SSE' ){
+			    return true;
+			}
+		}else{
+				   alert("nothing is checked");
+				   return false;
+				 }
+			 
+		
 	}
-
